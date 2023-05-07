b7edfce83371f3eb192e89ae818384bf37f708cd
belerico <federico.belotti@orobix.com>
2023-05-07 12:32:21 +0200

    Fix test + add reduction

--- fabricrl/algos/ppo/loss.py
+++ fabricrl/algos/ppo/loss.py
@@ -1,10 +1,19 @@
+from typing import Optional
+
 import torch
 import torch.nn.functional as F
 from torch import Tensor
 from torch.distributions import Distribution
 
 
-def policy_loss(dist: Distribution, actions: Tensor, logprobs: Tensor, advantages: Tensor, clip_coef: float) -> Tensor:
+def policy_loss(
+    dist: Distribution,
+    actions: Tensor,
+    logprobs: Tensor,
+    advantages: Tensor,
+    clip_coef: float,
+    reduction: Optional[str] = "mean",
+) -> Tensor:
     """Compute the policy loss for a batch of data, as described in equation (7) of the paper.
 
         - Compute the logprobs using the updated model for the actions taken.
@@ -13,7 +22,10 @@ def policy_loss(dist: Distribution, actions: Tensor, logprobs: Tensor, advantage
         - Use the ratio and advantages to compute the loss as per equation (7).
 
     Args:
-        dist (torch.distributions.Distribution): the policy distribution.
+        dist (Distribution): the policy distribution.
+        actions (Tensor): the actions sampled.
+        logprobs (Tensor): the log-probs of the actions.
+        advantages (Tensor): the advantages.
         clip_coef (float): the clipping coefficient.
 
     Returns:
@@ -25,8 +37,15 @@ def policy_loss(dist: Distribution, actions: Tensor, logprobs: Tensor, advantage
 
     pg_loss1 = advantages * ratio
     pg_loss2 = advantages * torch.clamp(ratio, 1 - clip_coef, 1 + clip_coef)
-    pg_loss = torch.min(pg_loss1, pg_loss2).mean()
-    return pg_loss
+    pg_loss = torch.min(pg_loss1, pg_loss2)
+    if reduction is None:
+        return pg_loss
+    elif reduction.lower() == "mean":
+        return pg_loss.mean()
+    elif reduction.lower() == "sum":
+        return pg_loss.sum()
+    else:
+        raise ValueError(f"Unrecognized reduction: {reduction}")
 
 
 def value_loss(
@@ -35,9 +54,10 @@ def value_loss(
     returns: Tensor,
     clip_coef: float,
     clip_vloss: bool,
+    reduction: Optional[str] = "mean",
 ) -> Tensor:
     if not clip_vloss:
         values_pred = new_values
     else:
         values_pred = old_values + torch.clamp(new_values - old_values, -clip_coef, clip_coef)
-    return F.mse_loss(values_pred, returns)
+    return F.mse_loss(values_pred, returns, reduction=reduction)
