f5ce48d9685af970595a530e1d59e47517cf252e
belerico <federico.belotti@orobix.com>
2023-05-10 13:38:41 +0200

    dry-run tests

--- tests/test_algos/test_algos.py
+++ tests/test_algos/test_algos.py
@@ -1,6 +1,7 @@
 import importlib
 import os
 import sys
+from contextlib import nullcontext
 from unittest import mock
 
 import pytest
@@ -14,7 +15,7 @@ def devices(request):
 
 @pytest.fixture()
 def standard_args():
-    return ["--num_envs=1", "--total_steps=64"]
+    return ["--num_envs=1", "--dry_run"]
 
 
 @pytest.fixture(autouse=True)
@@ -28,7 +29,12 @@ def mock_env_and_destroy(devices):
 @pytest.mark.timeout(60)
 def test_droq(standard_args):
     task = importlib.import_module("fabricrl.algos.droq.droq")
-    args = standard_args + ["--per_rank_batch_size=16", "--buffer_size=64", "--learning_starts=4", "--gradient_steps=1"]
+    args = standard_args + [
+        "--per_rank_batch_size=1",
+        f"--buffer_size={int(os.environ['LT_DEVICES'])}",
+        "--learning_starts=0",
+        "--gradient_steps=1",
+    ]
     with mock.patch.object(sys, "argv", [task.__file__] + args):
         for command in task.__all__:
             if command == "main":
@@ -38,17 +44,46 @@ def test_droq(standard_args):
 @pytest.mark.timeout(60)
 def test_sac(standard_args):
     task = importlib.import_module("fabricrl.algos.sac.sac")
-    args = standard_args + ["--per_rank_batch_size=16", "--buffer_size=64", "--learning_starts=4", "--gradient_steps=1"]
+    args = standard_args + [
+        "--per_rank_batch_size=1",
+        f"--buffer_size={int(os.environ['LT_DEVICES'])}",
+        "--learning_starts=0",
+        "--gradient_steps=1",
+    ]
     with mock.patch.object(sys, "argv", [task.__file__] + args):
         for command in task.__all__:
             if command == "main":
                 task.__dict__[command]()
+
+
+@pytest.mark.timeout(60)
+def test_sac_decoupled(standard_args):
+    task = importlib.import_module("fabricrl.algos.sac.sac_decoupled")
+    args = standard_args + [
+        "--per_rank_batch_size=1",
+        f"--buffer_size={int(os.environ['LT_DEVICES'])}",
+        "--learning_starts=0",
+        "--gradient_steps=1",
+    ]
+    with mock.patch.object(sys, "argv", [task.__file__] + args):
+        import torch.distributed.run as torchrun
+        from torch.distributed.elastic.multiprocessing.errors import ChildFailedError
+
+        for command in task.__all__:
+            if command == "main":
+                with pytest.raises(ChildFailedError) if os.environ["LT_DEVICES"] == "1" else nullcontext():
+                    torchrun_args = [
+                        f"--nproc_per_node={os.environ['LT_DEVICES']}",
+                        "--nnodes=1",
+                        "--standalone",
+                    ] + sys.argv
+                    torchrun.main(torchrun_args)
 
 
 @pytest.mark.timeout(60)
 def test_ppo(standard_args):
     task = importlib.import_module("fabricrl.algos.ppo.ppo")
-    args = standard_args + ["--rollout_steps=32", "--per_rank_batch_size=16"]
+    args = standard_args + ["--rollout_steps=1", "--per_rank_batch_size=1"]
     with mock.patch.object(sys, "argv", [task.__file__] + args):
         for command in task.__all__:
             if command == "main":
@@ -56,9 +91,51 @@ def test_ppo(standard_args):
 
 
 @pytest.mark.timeout(60)
+def test_ppo_decoupled(standard_args):
+    task = importlib.import_module("fabricrl.algos.ppo.ppo_decoupled")
+    args = standard_args + ["--rollout_steps=1", "--per_rank_batch_size=1"]
+    with mock.patch.object(sys, "argv", [task.__file__] + args):
+        import torch.distributed.run as torchrun
+        from torch.distributed.elastic.multiprocessing.errors import ChildFailedError
+
+        for command in task.__all__:
+            if command == "main":
+                with pytest.raises(ChildFailedError) if os.environ["LT_DEVICES"] == "1" else nullcontext():
+                    torchrun_args = [
+                        f"--nproc_per_node={os.environ['LT_DEVICES']}",
+                        "--nnodes=1",
+                        "--standalone",
+                    ] + sys.argv
+                    torchrun.main(torchrun_args)
+
+
+@pytest.mark.timeout(60)
+def test_ppo_atari(standard_args):
+    task = importlib.import_module("fabricrl.algos.ppo.ppo_atari")
+    args = standard_args + [
+        "--rollout_steps=1",
+        "--per_rank_batch_size=1",
+        "--env_id=BreakoutNoFrameskip-v4",
+    ]
+    with mock.patch.object(sys, "argv", [task.__file__] + args):
+        import torch.distributed.run as torchrun
+        from torch.distributed.elastic.multiprocessing.errors import ChildFailedError
+
+        for command in task.__all__:
+            if command == "main":
+                with pytest.raises(ChildFailedError) if os.environ["LT_DEVICES"] == "1" else nullcontext():
+                    torchrun_args = [
+                        f"--nproc_per_node={os.environ['LT_DEVICES']}",
+                        "--nnodes=1",
+                        "--standalone",
+                    ] + sys.argv
+                    torchrun.main(torchrun_args)
+
+
+@pytest.mark.timeout(60)
 def test_ppo_continuous(standard_args):
     task = importlib.import_module("fabricrl.algos.ppo_continuous.ppo_continuous")
-    args = standard_args + ["--rollout_steps=32", "--per_rank_batch_size=16"]
+    args = standard_args + ["--rollout_steps=1", "--per_rank_batch_size=1"]
     with mock.patch.object(sys, "argv", [task.__file__] + args):
         for command in task.__all__:
             if command == "main":
@@ -68,7 +145,7 @@ def test_ppo_continuous(standard_args):
 @pytest.mark.timeout(60)
 def test_ppo_recurrent(standard_args):
     task = importlib.import_module("fabricrl.algos.ppo_recurrent.ppo_recurrent")
-    args = standard_args + ["--rollout_steps=32", "--per_rank_batch_size=16"]
+    args = standard_args + ["--rollout_steps=1", "--per_rank_batch_size=1"]
     with mock.patch.object(sys, "argv", [task.__file__] + args):
         for command in task.__all__:
             if command == "main":
