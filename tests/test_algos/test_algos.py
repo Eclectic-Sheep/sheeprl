70abc1eb517cbb865f66cc0e3ba2811b107fe5e4
Michele Milesi <michele.milesi@orobix.com>
2023-05-18 17:02:55 +0000

    test: added test

--- tests/test_algos/test_algos.py
+++ tests/test_algos/test_algos.py
@@ -22,6 +22,11 @@ def standard_args():
     return ["--num_envs=1", "--dry_run"]
 
 
+@pytest.fixture(params=[True, False])
+def checkpoint_buffer(request):
+    return request.param
+
+
 @pytest.fixture(autouse=True)
 def mock_env_and_destroy(devices):
     with mock.patch.dict(os.environ, {"LT_ACCELERATOR": "cpu", "LT_DEVICES": str(devices)}) as _fixture:
@@ -30,11 +35,13 @@ def mock_env_and_destroy(devices):
         dist.destroy_process_group()
 
 
-def check_checkpoint(algo: str, target_keys: set):
+def check_checkpoint(algo: str, target_keys: set, checkpoint_buffer: bool = True):
     fabric = Fabric(accelerator="cpu")
     if not _is_using_cli():
         fabric.launch()
     project_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+    # check the presence of the checkpoint
     ckpt_path = f"{project_root}/logs/{algo}/"
     experiment_list = sorted(os.listdir(ckpt_path))
     assert len(experiment_list) > 0
@@ -42,12 +49,17 @@ def check_checkpoint(algo: str, target_keys: set):
     ckpt_path += os.listdir(ckpt_path)[-1] + "/version_0/checkpoint/"
     assert len(os.listdir(ckpt_path)) == 1
     state = fabric.load(ckpt_path + os.listdir(ckpt_path)[-1])
+
+    # the keys in the checkpoint must match with the expected keys
     ckpt_keys = set(state.keys())
-    assert len(ckpt_keys.intersection(target_keys)) == len(ckpt_keys)
+    assert len(ckpt_keys.intersection(target_keys)) == len(ckpt_keys) == len(target_keys)
+
+    # if checkpoint_buffer is false, then "rb" cannot be in the checkpoint keys
+    assert checkpoint_buffer or "rb" not in ckpt_keys
 
 
 @pytest.mark.timeout(60)
-def test_droq(standard_args):
+def test_droq(standard_args, checkpoint_buffer):
     task = importlib.import_module("sheeprl.algos.droq.droq")
     args = standard_args + [
         "--per_rank_batch_size=1",
@@ -55,19 +67,23 @@ def test_droq(standard_args):
         "--learning_starts=0",
         "--gradient_steps=1",
     ]
+    if checkpoint_buffer:
+        args.append("--checkpoint_buffer")
+
     with mock.patch.object(sys, "argv", [task.__file__] + args):
         for command in task.__all__:
             if command == "main":
                 task.__dict__[command]()
 
     with mock.patch.dict(os.environ, {"LT_ACCELERATOR": "cpu", "LT_DEVICES": str(1)}):
-        check_checkpoint(
-            "droq", {"agent", "qf_optimizer", "actor_optimizer", "alpha_optimizer", "args", "rb", "global_step"}
-        )
+        keys = {"agent", "qf_optimizer", "actor_optimizer", "alpha_optimizer", "args", "global_step"}
+        if checkpoint_buffer:
+            keys.add("rb")
+        check_checkpoint("droq", keys, checkpoint_buffer)
 
 
 @pytest.mark.timeout(60)
-def test_sac(standard_args):
+def test_sac(standard_args, checkpoint_buffer):
     task = importlib.import_module("sheeprl.algos.sac.sac")
     args = standard_args + [
         "--per_rank_batch_size=1",
@@ -75,25 +91,32 @@ def test_sac(standard_args):
         "--learning_starts=0",
         "--gradient_steps=1",
     ]
+    if checkpoint_buffer:
+        args.append("--checkpoint_buffer")
+
     with mock.patch.object(sys, "argv", [task.__file__] + args):
         for command in task.__all__:
             if command == "main":
                 task.__dict__[command]()
 
     with mock.patch.dict(os.environ, {"LT_ACCELERATOR": "cpu", "LT_DEVICES": str(1)}):
-        check_checkpoint(
-            "sac", {"agent", "qf_optimizer", "actor_optimizer", "alpha_optimizer", "args", "rb", "global_step"}
-        )
+        keys = {"agent", "qf_optimizer", "actor_optimizer", "alpha_optimizer", "args", "global_step"}
+        if checkpoint_buffer:
+            keys.add("rb")
+        check_checkpoint("sac", keys, checkpoint_buffer)
 
 
 @pytest.mark.timeout(60)
-def test_sac_decoupled(standard_args):
+def test_sac_decoupled(standard_args, checkpoint_buffer):
     task = importlib.import_module("sheeprl.algos.sac.sac_decoupled")
     args = standard_args + [
         "--per_rank_batch_size=1",
         "--learning_starts=0",
         "--gradient_steps=1",
     ]
+    if checkpoint_buffer:
+        args.append("--checkpoint_buffer")
+
     with mock.patch.object(sys, "argv", [task.__file__] + args):
         import torch.distributed.run as torchrun
         from torch.distributed.elastic.multiprocessing.errors import ChildFailedError
@@ -118,10 +141,10 @@ def test_sac_decoupled(standard_args):
 
     if os.environ["LT_DEVICES"] != "1":
         with mock.patch.dict(os.environ, {"LT_ACCELERATOR": "cpu", "LT_DEVICES": str(1)}):
-            check_checkpoint(
-                "sac_decoupled",
-                {"agent", "qf_optimizer", "actor_optimizer", "alpha_optimizer", "args", "rb", "global_step"},
-            )
+            keys = {"agent", "qf_optimizer", "actor_optimizer", "alpha_optimizer", "args", "global_step"}
+            if checkpoint_buffer:
+                keys.add("rb")
+            check_checkpoint("sac_decoupled", keys, checkpoint_buffer)
 
 
 @pytest.mark.timeout(60)
