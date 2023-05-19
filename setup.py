993526168fc6536599c130513d92631b25e0c36f
belerico <belo.fede@outlook.com>
2023-05-19 18:43:00 +0200

    Migration from poetry

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setups the project."""
+
+import pathlib
+
+from setuptools import setup
+
+
+CWD = pathlib.Path(__file__).absolute().parent
+
+
+def get_version():
+    """Gets the sheeprl version."""
+    path = CWD / "sheeprl" / "__init__.py"
+    content = path.read_text()
+
+    for line in content.splitlines():
+        if line.startswith("__version__"):
+            return line.strip().split()[-1].strip().strip('"')
+    raise RuntimeError("bad version data in __init__.py")
+
+
+setup(name="gymnasium", version=get_version())
