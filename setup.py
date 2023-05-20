00c1188f7d244d548c07cb50e24e4712c1cefc93
belerico <belo.fede@outlook.com>
2023-05-20 09:57:20 +0200

    [skip ci] Fix naming

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-"""Setups the project."""
+"""Inspired by https://github.com/Farama-Foundation/Gymnasium/blob/main/setup.py"""
 
 import pathlib
 
 from setuptools import setup
-
 
 CWD = pathlib.Path(__file__).absolute().parent
 
@@ -16,7 +15,7 @@ def get_version():
     for line in content.splitlines():
         if line.startswith("__version__"):
             return line.strip().split()[-1].strip().strip('"')
-    raise RuntimeError("bad version data in __init__.py")
+    raise RuntimeError("Bad version data in __init__.py")
 
 
-setup(name="gymnasium", version=get_version())
+setup(name="sheeprl", version=get_version())
