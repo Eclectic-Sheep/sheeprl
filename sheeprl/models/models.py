8da28f44630087b7bec7756286644eda167d2ab2
Davide Angioni <davide.angioni@orobix.com>
2023-05-23 15:26:57 +0200

    added CNN and DeCNN blocks, created helper functions for miniblock and layers creations

--- sheeprl/models/models.py
+++ sheeprl/models/models.py
@@ -2,34 +2,13 @@
 Adapted from: https://github.com/thu-ml/tianshou/blob/master/tianshou/utils/net/common.py
 """
 from math import prod
-from typing import Optional, Sequence, Type, Union, no_type_check
+from typing import Optional, Sequence, Union, no_type_check
 
 import torch
 import torch.nn.functional as F
 from torch import Tensor, nn
 
-from sheeprl.utils.model import ArgsType, ModuleType, miniblock
-
-
-def create_layers(layer_type: Union[ModuleType, Sequence[ModuleType]], layer_args: Optional[ArgsType], num_layers) -> Sequence[Optional[ModuleType]]:
-    if layer_type is None:
-        layers_list = [None] * num_layers
-        args_list = [None] * num_layers
-        return layers_list, args_list
-
-    if isinstance(layer_type, Sequence):
-        assert len(layer_type) == num_layers
-        layers_list = layer_type
-        if isinstance(layer_args, Sequence):
-            assert len(layer_args) == num_layers
-            args_list = layer_args
-        else:
-            args_list = [layer_args for _ in range(num_layers)]
-    else:
-        layers_list = [layer_type for _ in range(num_layers)]
-        args_list = [layer_args for _ in range(num_layers)]
-    return layers_list, args_list
-
+from sheeprl.utils.model import ArgsType, ModuleType, create_layers, miniblock
 
 
 class MLP(nn.Module):
@@ -37,9 +16,9 @@ class MLP(nn.Module):
 
     Args:
         input_dims (Union[int, Sequence[int]]): dimensions of the input vector.
-        output_dim (int, optional): dimension of the output vector. If set to 0, there
-            is no final linear layer.
-            Defaults to 0.
+        output_dim (int, optional): dimension of the output vector. If set to None, there
+            is no final linear layer. Else, a final linear layer is added.
+            Defaults to None.
         hidden_sizes (Sequence[int], optional): shape of MLP passed in as a list, not including
             input_dims and output_dim.
         dropout_layer (Union[ModuleType, Sequence[ModuleType]], optional): which dropout layer to be used
@@ -58,10 +37,6 @@ class MLP(nn.Module):
             can be both the same activation for all layers if a single ``nn.Module`` is passed, or different
             activations for different layers if a list is passed.
             Defaults to ``nn.ReLU``.
-        device (Union[str, int, torch.device], optional): which device to create this model on.
-            Defaults to "cpu".
-        linear_layer (ModuleType, optional): which linear layer to use.
-            Defaults to ``nn.Linear``.
         flatten_dim (int, optional): whether to flatten input data. The flatten dimension starts from 1.
             Defaults to True.
     """
@@ -69,57 +44,39 @@ class MLP(nn.Module):
     def __init__(
         self,
         input_dims: Union[int, Sequence[int]],
-        output_dim: int = 0,
+        output_dim: Optional[int] = None,
         hidden_sizes: Sequence[int] = (),
+        layer_args: Optional[ArgsType] = None,
         dropout_layer: Optional[Union[ModuleType, Sequence[ModuleType]]] = None,
         dropout_args: Optional[ArgsType] = None,
         norm_layer: Optional[Union[ModuleType, Sequence[ModuleType]]] = None,
         norm_args: Optional[ArgsType] = None,
         activation: Optional[Union[ModuleType, Sequence[ModuleType]]] = nn.ReLU,
         act_args: Optional[ArgsType] = None,
-        linear_layer: Type[nn.Linear] = nn.Linear,
         flatten_dim: Optional[int] = 1,
     ) -> None:
         super().__init__()
-        dropout_layer_list, dropout_args_list = create_layers(dropout_layer, dropout_args, len(hidden_sizes))
+        num_layers = len(hidden_sizes)
+        if num_layers < 1 and output_dim is None:
+            raise ValueError("The number of layers should be at least 1.")
 
-        if norm_layer:
-            if isinstance(norm_layer, list):
-                assert len(norm_layer) == len(hidden_sizes)
-                norm_layer_list = norm_layer
-                if isinstance(norm_args, list):
-                    assert len(norm_args) == len(hidden_sizes)
-                    norm_args_list = norm_args
-                else:
-                    norm_args_list = [norm_args for _ in range(len(hidden_sizes))]
-            else:
-                norm_layer_list = [norm_layer for _ in range(len(hidden_sizes))]
-                norm_args_list = [norm_args for _ in range(len(hidden_sizes))]
+        dropout_layer_list, dropout_args_list = create_layers(dropout_layer, dropout_args, num_layers)
+        norm_layer_list, norm_args_list = create_layers(norm_layer, norm_args, num_layers)
+        activation_list, act_args_list = create_layers(activation, act_args, num_layers)
+
+        if isinstance(layer_args, list):
+            layer_args_list = layer_args
         else:
-            norm_layer_list = [None] * len(hidden_sizes)
-            norm_args_list = [None] * len(hidden_sizes)
-        if activation:
-            if isinstance(activation, list):
-                assert len(activation) == len(hidden_sizes)
-                activation_list = activation
-                if isinstance(act_args, list):
-                    assert len(act_args) == len(hidden_sizes)
-                    act_args_list = act_args
-                else:
-                    act_args_list = [act_args for _ in range(len(hidden_sizes))]
-            else:
-                activation_list = [activation for _ in range(len(hidden_sizes))]
-                act_args_list = [act_args for _ in range(len(hidden_sizes))]
-        else:
-            activation_list = [None] * len(hidden_sizes)
-            act_args_list = [None] * len(hidden_sizes)
+            layer_args_list = [layer_args] * num_layers
+
         if isinstance(input_dims, int):
             input_dims = [input_dims]
         hidden_sizes = [prod(input_dims)] + list(hidden_sizes)
         model = []
-        for in_dim, out_dim, drop, drop_args, norm, norm_args, activ, act_args in zip(
+        for in_dim, out_dim, l_args, drop, drop_args, norm, norm_args, activ, act_args in zip(
             hidden_sizes[:-1],
             hidden_sizes[1:],
+            layer_args_list,
             dropout_layer_list,
             dropout_args_list,
             norm_layer_list,
@@ -127,9 +84,10 @@ class MLP(nn.Module):
             activation_list,
             act_args_list,
         ):
-            model += miniblock(in_dim, out_dim, drop, drop_args, norm, norm_args, activ, act_args, linear_layer)
-        if output_dim > 0:
-            model += [linear_layer(hidden_sizes[-1], output_dim)]
+            model += miniblock(in_dim, out_dim, nn.Linear, l_args, drop, drop_args, norm, norm_args, activ, act_args)
+        if output_dim is not None:
+            model += [nn.Linear(hidden_sizes[-1], output_dim)]
+
         self._output_dim = output_dim or hidden_sizes[-1]
         self._model = nn.Sequential(*model)
         self._flatten_dim = flatten_dim
@@ -153,7 +111,171 @@ class MLP(nn.Module):
         return self.model(obs)
 
 
-class NatureCNN(nn.Module):
+class CNN(nn.Module):
+    """Simple CNN backbone.
+
+    Args:
+        input_channels (int): dimensions of the input channels.
+        hidden_channels (Sequence[int], optional): intermediate number of channels for the CNN, including the output channels.
+        dropout_layer (Union[ModuleType, Sequence[ModuleType]], optional): which dropout layer to be used
+            before activation (possibly before the normalization layer), e.g., ``nn.Dropout``.
+            You can also pass a list of dropout modules with the same length
+            of hidden_sizes to use different dropout modules in different layers.
+            If None, then no dropout layer is used.
+            Defaults to None.
+        norm_layer (Union[ModuleType, Sequence[ModuleType]], optional): which normalization layer to be used
+            before activation, e.g., ``nn.LayerNorm`` and ``nn.BatchNorm1d``.
+            You can also pass a list of normalization modules with the same length
+            of hidden_sizes to use different normalization modules in different layers.
+            If None, then no normalization layer is used.
+            Defaults to None.
+        activation (Union[ModuleType, Sequence[ModuleType]], optional): which activation to use after each layer,
+            can be both the same activation for all layers if a single ``nn.Module`` is passed, or different
+            activations for different layers if a list is passed.
+            Defaults to ``nn.ReLU``.
+    """
+
+    def __init__(
+        self,
+        input_channels: int,
+        hidden_channels: Sequence[int],
+        layer_args: ArgsType = None,
+        dropout_layer: Optional[Union[ModuleType, Sequence[ModuleType]]] = None,
+        dropout_args: Optional[ArgsType] = None,
+        norm_layer: Optional[Union[ModuleType, Sequence[ModuleType]]] = None,
+        norm_args: Optional[ArgsType] = None,
+        activation: Optional[Union[ModuleType, Sequence[ModuleType]]] = nn.ReLU,
+        act_args: Optional[ArgsType] = None,
+    ) -> None:
+        super().__init__()
+        num_layers = len(hidden_channels)
+        if num_layers < 1:
+            raise ValueError("The number of layers should be at least 1.")
+
+        dropout_layer_list, dropout_args_list = create_layers(dropout_layer, dropout_args, num_layers)
+        norm_layer_list, norm_args_list = create_layers(norm_layer, norm_args, num_layers)
+        activation_list, act_args_list = create_layers(activation, act_args, num_layers)
+
+        if isinstance(layer_args, list):
+            layer_args_list = layer_args
+        else:
+            layer_args_list = [layer_args] * num_layers
+
+        hidden_sizes = [input_channels] + list(hidden_channels)
+        model = []
+        for in_dim, out_dim, l_args, drop, drop_args, norm, norm_args, activ, act_args in zip(
+            hidden_sizes[:-1],
+            hidden_sizes[1:],
+            layer_args_list,
+            dropout_layer_list,
+            dropout_args_list,
+            norm_layer_list,
+            norm_args_list,
+            activation_list,
+            act_args_list,
+        ):
+            model += miniblock(in_dim, out_dim, nn.Conv2d, l_args, drop, drop_args, norm, norm_args, activ, act_args)
+
+        self._output_dim = hidden_sizes[-1]
+        self._model = nn.Sequential(*model)
+
+    @property
+    def model(self) -> nn.Module:
+        return self._model
+
+    @property
+    def output_dim(self) -> int:
+        return self._output_dim
+
+    @no_type_check
+    def forward(self, obs: Tensor) -> Tensor:
+        return self.model(obs)
+
+
+class DeCNN(nn.Module):
+    """Simple DeCNN backbone.
+
+    Args:
+        input_channels (int): dimensions of the input channels.
+        hidden_channels (Sequence[int], optional): intermediate number of channels for the CNN, including the output channels.
+        dropout_layer (Union[ModuleType, Sequence[ModuleType]], optional): which dropout layer to be used
+            before activation (possibly before the normalization layer), e.g., ``nn.Dropout``.
+            You can also pass a list of dropout modules with the same length
+            of hidden_sizes to use different dropout modules in different layers.
+            If None, then no dropout layer is used.
+            Defaults to None.
+        norm_layer (Union[ModuleType, Sequence[ModuleType]], optional): which normalization layer to be used
+            before activation, e.g., ``nn.LayerNorm`` and ``nn.BatchNorm1d``.
+            You can also pass a list of normalization modules with the same length
+            of hidden_sizes to use different normalization modules in different layers.
+            If None, then no normalization layer is used.
+            Defaults to None.
+        activation (Union[ModuleType, Sequence[ModuleType]], optional): which activation to use after each layer,
+            can be both the same activation for all layers if a single ``nn.Module`` is passed, or different
+            activations for different layers if a list is passed.
+            Defaults to ``nn.ReLU``.
+    """
+
+    def __init__(
+        self,
+        input_channels: int,
+        hidden_channels: Sequence[int] = (),
+        layer_args: ArgsType = None,
+        dropout_layer: Optional[Union[ModuleType, Sequence[ModuleType]]] = None,
+        dropout_args: Optional[ArgsType] = None,
+        norm_layer: Optional[Union[ModuleType, Sequence[ModuleType]]] = None,
+        norm_args: Optional[ArgsType] = None,
+        activation: Optional[Union[ModuleType, Sequence[ModuleType]]] = nn.ReLU,
+        act_args: Optional[ArgsType] = None,
+    ) -> None:
+        super().__init__()
+        num_layers = len(hidden_channels)
+        if num_layers < 1:
+            raise ValueError("The number of layers should be at least 1.")
+
+        dropout_layer_list, dropout_args_list = create_layers(dropout_layer, dropout_args, num_layers)
+        norm_layer_list, norm_args_list = create_layers(norm_layer, norm_args, num_layers)
+        activation_list, act_args_list = create_layers(activation, act_args, num_layers)
+
+        if isinstance(layer_args, list):
+            layer_args_list = layer_args
+        else:
+            layer_args_list = [layer_args] * num_layers
+
+        hidden_sizes = [input_channels] + list(hidden_channels)
+        model = []
+        for in_dim, out_dim, l_args, drop, drop_args, norm, norm_args, activ, act_args in zip(
+            hidden_sizes[:-1],
+            hidden_sizes[1:],
+            layer_args_list,
+            dropout_layer_list,
+            dropout_args_list,
+            norm_layer_list,
+            norm_args_list,
+            activation_list,
+            act_args_list,
+        ):
+            model += miniblock(
+                in_dim, out_dim, nn.ConvTranspose2d, l_args, drop, drop_args, norm, norm_args, activ, act_args
+            )
+
+        self._output_dim = hidden_sizes[-1]
+        self._model = nn.Sequential(*model)
+
+    @property
+    def model(self) -> nn.Module:
+        return self._model
+
+    @property
+    def output_dim(self) -> int:
+        return self._output_dim
+
+    @no_type_check
+    def forward(self, obs: Tensor) -> Tensor:
+        return self.model(obs)
+
+
+class NatureCNN(CNN):
     """CNN from DQN Nature paper: Mnih, Volodymyr, et al. "Human-level control through deep reinforcement learning."
     Nature 518.7540 (2015): 529-533.
 
@@ -167,22 +289,22 @@ class NatureCNN(nn.Module):
     """
 
     def __init__(self, in_channels: int, features_dim: int, screen_size: int = 64):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        super().__init__(
+            in_channels,
+            [32, 64, 64],
+            layer_args=[
+                {"kernel_size": 8, "stride": 4},
+                {"kernel_size": 4, "stride": 2},
+                {"kernel_size": 3, "stride": 1},
+            ],
+        )
+
         with torch.no_grad():
-            x = F.relu(
-                self.conv1(torch.rand(1, in_channels, screen_size, screen_size, device=self.conv1.weight.device))
-            )
-            x = F.relu(self.conv2(x))
-            x = F.relu(self.conv3(x))
+            x = self.model(torch.rand(1, in_channels, screen_size, screen_size, device=self.model[0].weight.device))
             out_dim = x.flatten(1).shape[1]
         self.fc = nn.Linear(out_dim, features_dim)
 
     def forward(self, x: Tensor) -> Tensor:
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        x = self.model(x)
         x = F.relu(self.fc(x.flatten(1)))
         return x
--- sheeprl/utils/model.py
+++ sheeprl/utils/model.py
@@ -1,16 +1,22 @@
 """
 Adapted from: https://github.com/thu-ml/tianshou/blob/master/tianshou/utils/net/common.py
 """
-from typing import Any, Dict, List, Optional, Sequence, Tuple, Type, Union
+from typing import Any, Dict, List, Optional, Tuple, Type, Union
 
 from torch import nn
 
-ModuleType = Type[nn.Module]
-ArgType = Union[Tuple[Any, ...], Dict[Any, Any]]
-ArgsType = Union[ArgType, Sequence[ArgType]]
+ModuleType = Optional[Type[nn.Module]]
+ArgType = Optional[Tuple[Any, ...], Dict[Any, Any]]
+ArgsType = Union[ArgType, List[ArgType]]
 
 
 def create_layer_with_args(layer_type: ModuleType, layer_args: Optional[ArgType]) -> nn.Module:
+    """Create a single layer with given layer type and arguments.
+
+    Args:
+        layer_type (ModuleType): the type of the layer to be created.
+        layer_args (ArgType, optional): the arguments to be passed to the layer.
+    """
     if isinstance(layer_args, tuple):
         return layer_type(*layer_args)
     elif isinstance(layer_args, dict):
@@ -23,18 +29,40 @@ def create_layer_with_args(layer_type: ModuleType, layer_args: Optional[ArgType]
 
 def miniblock(
     input_size: int,
-    output_size: int = 0,
-    layer_type: Type[nn.Linear] = nn.Linear,
-    layer_args: Optional[Union[Tuple[Any, ...], Dict[Any, Any]]] = None,
-    dropout_layer: Optional[ModuleType] = None,
-    dropout_args: Optional[Union[Tuple[Any, ...], Dict[Any, Any]]] = None,
-    norm_layer: Optional[ModuleType] = None,
-    norm_args: Optional[Union[Tuple[Any, ...], Dict[Any, Any]]] = None,
-    activation: Optional[ModuleType] = None,
-    act_args: Optional[Union[Tuple[Any, ...], Dict[Any, Any]]] = None,
+    output_size: int,
+    layer_type: Type[nn.Module] = nn.Linear,
+    layer_args: ArgType = None,
+    dropout_layer: ModuleType = None,
+    dropout_args: ArgType = None,
+    norm_layer: ModuleType = None,
+    norm_args: ArgType = None,
+    activation: ModuleType = None,
+    act_args: ArgType = None,
 ) -> List[nn.Module]:
-    """Construct a miniblock with given input/output-size, norm layer and \
-    activation function."""
+    """Construct a miniblock with given input/output-size, dropout layer, norm layer and activation function.
+
+    Based on Tianshou's miniblock function (https://github.com/thu-ml/tianshou/blob/master/tianshou/utils/net/common.py).
+
+    Args:
+        input_size (int): the input size of the miniblock (in_features for Linear and in_channels for Conv2d).
+        output_size (int): the output size of the miniblock.
+        layer_type (Type[nn.Linear], optional): the type of the layer to be created. Defaults to nn.Linear.
+        layer_args (ArgType, optional): the arguments to be passed to the layer.
+            Defaults to None.
+        dropout_layer (ModuleType, optional): the type of the dropout layer to be created. Defaults to None.
+        dropout_args (ArgType, optional): the arguments to be passed to the dropout
+            layer. Defaults to None.
+        norm_layer (ModuleType, optional): the type of the norm layer to be created. Defaults to None.
+        norm_args (ArgType, optional): the arguments to be passed to the norm layer.
+            Defaults to None.
+        activation (ModuleType, optional): the type of the activation function to be created.
+            Defaults to None.
+        act_args (Tuple[Any, ...] | Dict[Any, Any] | None, optional): the arguments to be passed to the activation
+            function. Defaults to None.
+
+    Returns:
+        List[nn.Module]: the miniblock as a list of layers.
+    """
     if layer_args is None:
         layers: List[nn.Module] = [layer_type(input_size, output_size)]
     elif isinstance(layer_args, tuple):
@@ -53,6 +81,54 @@ def miniblock(
     if activation is not None:
         layers += [create_layer_with_args(activation, act_args)]
     return layers
+
+
+def create_layers(
+    layer_type: [ModuleType, List[ModuleType]], layer_args: Optional[ArgsType], num_layers: int
+) -> Tuple[List[ModuleType], ArgsType]:
+    """Create a list of layers with given layer type and arguments.
+
+    If a layer_type is not specified, then the lists will be filled with None. If the layer type or the layer arguments
+    are specified only once, they will be cast to a sequence of length num_layers.
+
+    Args:
+        layer_type (Union[ModuleType, Sequence[ModuleType]]): the type of the layer to be created.
+        layer_args (ArgsType, optional): the arguments to be passed to the layer.
+        num_layers (int): the number of layers to be created.
+
+    Returns:
+        Tuple[Sequence[ModuleType], ArgsType]: a list of layers and a list of args.
+
+    Examples:
+        >>> create_layers(nn.Linear, None, 3)
+        ([nn.Linear, nn.Linear, nn.Linear], [None, None, None])
+
+        >>> create_layers(nn.Linear, {"arg1":3, "arg2": "foo"}, 3)
+        ([nn.Linear, nn.Linear, nn.Linear], [{'arg1': 3, 'arg2': 'foo'}, {'arg1': 3, 'arg2': 'foo'}, {'arg1': 3, 'arg2': 'foo'}])
+
+        >>> create_layers([nn.Linear, nn.Conv2d], [{"bias":False}, {"kernel_size": 5, "bias": True}], 2)
+        ([nn.Linear, nn.Conv2d], [{'bias': False}, {'kernel_size':5, 'bias': True}])
+
+        >>> create_layers([nn.Linear, nn.Linear], (64, 10), 2)
+        ([nn.Linear, nn.Linear], [(64, 10), (64, 10)])
+    """
+    if layer_type is None:
+        layers_list = [None] * num_layers
+        args_list = [None] * num_layers
+        return layers_list, args_list
+
+    if isinstance(layer_type, list):
+        assert len(layer_type) == num_layers
+        layers_list = layer_type
+        if isinstance(layer_args, list):
+            assert len(layer_args) == num_layers
+            args_list = layer_args
+        else:
+            args_list = [layer_args for _ in range(num_layers)]
+    else:
+        layers_list = [layer_type for _ in range(num_layers)]
+        args_list = [layer_args for _ in range(num_layers)]
+    return layers_list, args_list
 
 
 def per_layer_ortho_init_weights(module: nn.Module, gain: float = 1.0, bias: float = 0.0):
