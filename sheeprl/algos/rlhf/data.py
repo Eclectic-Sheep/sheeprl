240e7e3207cb5653f414d1f75462935d4b944194
rcmalli <refikcan.malli@orobix.com>
2023-05-31 08:35:00 +0000

    feat(RLHF): update collate functions

--- sheeprl/algos/rlhf/data.py
+++ sheeprl/algos/rlhf/data.py
@@ -12,16 +12,33 @@ class SFTCollate:
         input_ids = [item["input_ids"].type(torch.int64) for item in batch]
         targets = [item["targets"].type(torch.int64) for item in batch]
 
-        # attention_mask
-        attention_mask = [torch.ones_like(x) for x in input_ids]
-
         # Use PyTorch's pad_sequence function
         input_ids = pad_sequence(input_ids, batch_first=True, padding_value=self.pad_value)
-        attention_mask = pad_sequence(attention_mask, batch_first=True, padding_value=0)
         targets = pad_sequence(targets, batch_first=True, padding_value=self.ignore_index)
+        attention_mask = input_ids.ne(self.pad_value).type(torch.int64)
 
         return {
             "input_ids": input_ids,
             "attention_mask": attention_mask,
             "targets": targets,
+        }
+
+class RMCollate:
+    def __init__(self, dim=1, pad_value=0, ignore_index=-1):
+        self.dim = dim
+        self.pad_value = pad_value
+        self.ignore_index = ignore_index
+
+    def __call__(self, batch):
+        input_ids = [item["chosen_input_ids"].type(torch.int64) for item in batch]
+        input_ids += [item["rejected_input_ids"].type(torch.int64) for item in batch]
+        # Use PyTorch's pad_sequence function
+        input_ids = pad_sequence(input_ids, batch_first=True, padding_value=self.pad_value)
+        attention_mask = input_ids.ne(self.pad_value).type(torch.int64)
+
+        return {
+            "chosen_input_ids": input_ids[: len(batch)],
+            "rejected_input_ids": input_ids[len(batch) :],
+            "chosen_attention_mask": attention_mask[: len(batch)],
+            "rejected_attention_mask": attention_mask[len(batch) :],
         }
