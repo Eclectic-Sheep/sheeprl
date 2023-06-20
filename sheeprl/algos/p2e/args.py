0aee1d581c1351f7ed06037e7054515e4104481d
belerico <belo.fede@outlook.com>
2023-06-20 11:51:58 +0200

    Fix p2e to be conditioned on both recurrent and stochastic

--- sheeprl/algos/p2e/args.py
+++ sheeprl/algos/p2e/args.py
@@ -11,8 +11,8 @@ class P2EArgs(DreamerV1Args):
     recurrent_state_size: int = Arg(default=400, help="the dimension of the recurrent state")
 
     # new args
-    num_ensembles: int = Arg(default=5, help="the number of the ensembles to compute the intrinsic reward")
-    ensemble_lr: float = Arg(default=1e-3, help="the learning rate of the optimizer of the ensembles")
-    ensemble_eps: float = Arg(default=1e-4, help="the epsilon of the Adam optimizer of the ensembles")
-    ensemble_clip_gradients: float = Arg(default=1000, help="how much to clip the gradient norms of the ensembles")
-    intrinsic_reward_multiplier: float = Arg(default=10000, help="how much scale the intrinsic rewards")
+    num_ensembles: int = Arg(default=10, help="the number of the ensembles to compute the intrinsic reward")
+    ensemble_lr: float = Arg(default=3e-4, help="the learning rate of the optimizer of the ensembles")
+    ensemble_eps: float = Arg(default=1e-5, help="the epsilon of the Adam optimizer of the ensembles")
+    ensemble_clip_gradients: float = Arg(default=100, help="how much to clip the gradient norms of the ensembles")
+    intrinsic_reward_multiplier: float = Arg(default=1, help="how much scale the intrinsic rewards")
