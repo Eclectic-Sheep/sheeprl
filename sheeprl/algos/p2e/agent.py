0485325af39595e43c265acc47387cecf65cf0b7
belerico <federico.belotti@orobix.com>
2023-06-26 15:11:24 +0000

    Fix imports

--- sheeprl/algos/p2e/agent.py
+++ sheeprl/algos/p2e/agent.py
@@ -8,7 +8,6 @@ from torch import Tensor, nn
 from sheeprl.algos.dreamer_v1.agent import RSSM, Actor, Encoder, RecurrentModel, WorldModel
 from sheeprl.algos.dreamer_v1.args import DreamerV1Args
 from sheeprl.algos.dreamer_v1.utils import init_weights
-from sheeprl.algos.p2e.agent import RSSMP2E
 from sheeprl.models.models import MLP, DeCNN
 
 
