461808e3d28bde042b8730d5e4cc8186f90be007
belerico <belo.fede@outlook.com>
2023-06-30 16:29:02 +0200

    Add dreamer-v2 tests

--- tests/test_algos/test_algos.py
+++ tests/test_algos/test_algos.py
@@ -525,3 +525,52 @@ def test_p2e(standard_args, env_id, checkpoint_buffer, start_time):
             keys.add("rb")
         check_checkpoint(ckpt_path, keys, checkpoint_buffer)
         shutil.rmtree("pytest_" + start_time)
+
+
+@pytest.mark.timeout(60)
+@pytest.mark.parametrize("env_id", ["AdventureNoFrameskip-v4", "CarRacing-v2"])
+@pytest.mark.parametrize("checkpoint_buffer", [True, False])
+def test_dreamer_v2(standard_args, env_id, checkpoint_buffer, start_time):
+    task = importlib.import_module("sheeprl.algos.dreamer_v2.dreamer_v2")
+    root_dir = os.path.join("pytest_" + start_time, "dreamer_v2", os.environ["LT_DEVICES"])
+    run_name = "checkpoint_buffer" if checkpoint_buffer else "no_checkpoint_buffer"
+    ckpt_path = os.path.join(root_dir, run_name)
+    version = 0 if not os.path.isdir(ckpt_path) else len(os.listdir(ckpt_path))
+    ckpt_path = os.path.join(ckpt_path, f"version_{version}", "checkpoint")
+    args = standard_args + [
+        "--per_rank_batch_size=1",
+        "--per_rank_sequence_length=1",
+        f"--buffer_size={int(os.environ['LT_DEVICES'])}",
+        "--learning_starts=0",
+        "--gradient_steps=1",
+        "--env_id=" + env_id,
+        "--root_dir=" + root_dir,
+        "--run_name=" + run_name,
+        "--dense_units=8",
+        "--cnn_channels_multiplier=2",
+    ]
+    if checkpoint_buffer:
+        args.append("--checkpoint_buffer")
+
+    with mock.patch.object(sys, "argv", [task.__file__] + args):
+        for command in task.__all__:
+            if command == "main":
+                task.__dict__[command]()
+
+    with mock.patch.dict(os.environ, {"LT_ACCELERATOR": "cpu", "LT_DEVICES": str(1)}):
+        keys = {
+            "world_model",
+            "actor",
+            "critic",
+            "world_optimizer",
+            "actor_optimizer",
+            "critic_optimizer",
+            "expl_decay_steps",
+            "args",
+            "global_step",
+            "batch_size",
+        }
+        if checkpoint_buffer:
+            keys.add("rb")
+        check_checkpoint(ckpt_path, keys, checkpoint_buffer)
+        shutil.rmtree("pytest_" + start_time)
