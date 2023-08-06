db6695053644a3babba1739b0534f44364f1664e
belerico <federico.belotti@orobix.com>
2023-08-06 16:43:20 +0000

    Add RestartOnException wrapper

--- sheeprl/envs/wrappers.py
+++ sheeprl/envs/wrappers.py
@@ -1,3 +1,6 @@
+import time
+from typing import Any, Callable, Dict, Optional, SupportsFloat
+
 import gymnasium as gym
 import numpy as np
 
@@ -62,3 +65,55 @@ class ActionRepeat(gym.Wrapper):
             total_reward += reward
             current_step += 1
         return obs, total_reward, done, truncated, info
+
+
+class RestartOnException(gym.Wrapper):
+    def __init__(self, env_fn: Callable[..., gym.Env], exceptions=(Exception,), window=300, maxfails=2, wait=20):
+        if not isinstance(exceptions, (tuple, list)):
+            exceptions = [exceptions]
+        self._env_fn = env_fn
+        self._exceptions = tuple(exceptions)
+        self._window = window
+        self._maxfails = maxfails
+        self._wait = wait
+        self._last = time.time()
+        self._fails = 0
+        super().__init__(self._env_fn())
+
+    def step(self, action) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+        try:
+            return self.env.step(action)
+        except self._exceptions as e:
+            if time.time() > self._last + self._window:
+                self._last = time.time()
+                self._fails = 1
+            else:
+                self._fails += 1
+            if self._fails > self._maxfails:
+                raise RuntimeError(f"The env crashed too many times: {self._fails}")
+            gym.logger.warn(f"STEP - Restarting env after crash with {type(e).__name__}: {e}")
+            time.sleep(self._wait)
+            self.env = self._env_fn()
+            new_obs, info = self.env.reset()
+            info.update({"restart_on_exception": True})
+            return new_obs, 0.0, False, False, info
+
+    def reset(
+        self, *, seed: Optional[int] = None, options: Optional[Dict[str, Any]] = None
+    ) -> tuple[Any, Dict[str, Any]]:
+        try:
+            return self.env.reset(seed=seed, options=options)
+        except self._exceptions as e:
+            if time.time() > self._last + self._window:
+                self._last = time.time()
+                self._fails = 1
+            else:
+                self._fails += 1
+            if self._fails > self._maxfails:
+                raise RuntimeError(f"The env crashed too many times: {self._fails}")
+            gym.logger.warn(f"RESET - Restarting env after crash with {type(e).__name__}: {e}")
+            time.sleep(self._wait)
+            self.env = self._env_fn()
+            new_obs, info = self.env.reset()
+            info.update({"restart_on_exception": True})
+            return new_obs, info
