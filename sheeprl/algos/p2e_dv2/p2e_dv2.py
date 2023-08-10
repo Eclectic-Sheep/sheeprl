f73c50674fc2b199386a5079c8d5c78535d8128c
belerico_t <federico.belotti@orobix.com>
2023-08-10 17:39:17 +0200

    [skip-ci] Removed unused stats-saving

--- sheeprl/algos/p2e_dv2/p2e_dv2.py
+++ sheeprl/algos/p2e_dv2/p2e_dv2.py
@@ -6,7 +6,6 @@ from dataclasses import asdict
 from typing import Dict, Sequence
 
 import gymnasium as gym
-import jsonlines
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -725,11 +724,6 @@ def main():
             max_decay_steps=max_step_expl_decay,
         )
 
-    # Stats file info
-    if "minedojo" in args.env_id:
-        stats_dir = os.path.join(log_dir, "stats")
-        os.makedirs(stats_dir, exist_ok=True)
-        stats_filename = os.path.join(stats_dir, "stats.jsonl")
     # Exploration
     exploration_updates = (
         int(args.exploration_steps // (fabric.world_size * args.action_repeat)) if not args.dry_run else 4
@@ -797,18 +791,6 @@ def main():
                     real_actions = torch.cat(real_actions, -1).cpu().numpy()
                 else:
                     real_actions = np.array([real_act.cpu().argmax(dim=-1).numpy() for real_act in real_actions])
-
-        # Save stats
-        if "minedojo" in args.env_id:
-            with jsonlines.open(stats_filename, mode="a") as writer:
-                writer.write(
-                    {
-                        "life_stats": infos["life_stats"],
-                        "location_stats": infos["location_stats"],
-                        "action": real_actions.tolist(),
-                        "biomeid": infos["biomeid"],
-                    }
-                )
 
         step_data["is_first"] = copy.deepcopy(step_data["dones"])
         o, rewards, dones, truncated, infos = envs.step(real_actions.reshape(envs.action_space.shape))
