96aa21980eb7f015bcb645f5674675c590a58c65
belerico_t <federico.belotti@orobix.com>
2023-09-08 15:08:30 +0200

    Add IndependentMeanMetric

--- sheeprl/utils/metric.py
+++ sheeprl/utils/metric.py
@@ -1,8 +1,13 @@
 from collections import deque
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, Optional, Sequence, Union
 
 import torch
+import torch.distributed as dist
+from lightning.fabric.utilities.distributed import _distributed_available
+from torch import Tensor
+from torch.distributed.distributed_c10d import ProcessGroup
 from torchmetrics import Metric
+from torchmetrics.aggregation import CatMetric
 
 
 class MetricAggregatorException(Exception):
@@ -86,6 +91,80 @@ class MetricAggregator:
                 if v._update_called:
                     reduced_metrics[k] = reduced.tolist()
         return reduced_metrics
+
+
+class IndependentMeanMetric:
+    def __init__(
+        self,
+        names: Sequence[str],
+        device: Union[str, torch.device] = "cpu",
+        process_group: Optional[ProcessGroup] = None,
+    ) -> None:
+        """Collection of N independent mean metrics, where `N` is given by the
+        length of the `names` parameter. This metric is useful when one wants to
+        maintain averages of some quantities, while still being able to broadcast them
+        to all the processes in a `torch.distributed` group.
+
+        Args:
+            names (Sequence[str]): the names of the metrics.
+            device (Union[str, torch.device], optional): the device where the metrics reside.
+                Defaults to "cpu".
+            process_group (Optional[ProcessGroup], optional): the distributed process group.
+                Defaults to None.
+        """
+        super().__init__()
+        if len(names) <= 0:
+            raise ValueError(f"`names` length must be greater than 0: got {len(names)}")
+        self._names = names
+        self._device = device
+        self._metrics: dict[str, Metric] = {}
+        for n in names:
+            m = CatMetric(sync_on_compute=False)
+            self._metrics[n] = m.to(self._device)
+        self._process_group = process_group if process_group is not None else torch.distributed.group.WORLD
+
+    def update(self, value: float, name: str) -> None:
+        self._metrics[name].update(value)
+
+    @torch.no_grad()
+    def compute(self) -> Dict[str, Tensor]:
+        """Compute the means, one for every metric. The metrics are first broadcasted
+
+        Returns:
+            Dict[str, Tensor]: _description_
+        """
+        computed_metrics = {}
+        for k, v in self._metrics.items():
+            computed_v = v.compute()
+            if not isinstance(computed_v, Tensor):
+                computed_metrics[k] = torch.tensor(computed_v, device=self._device)
+            else:
+                computed_metrics[k] = computed_v
+        if not _distributed_available():
+            return computed_metrics
+        gathered_data = [None for _ in range(dist.get_world_size(self._process_group))]
+        dist.all_gather_object(gathered_data, computed_metrics, group=self._process_group)
+        return_data = gathered_data[0]
+        for rank in range(1, len(gathered_data)):
+            for k, rank_v in gathered_data[rank].items():
+                if isinstance(rank_v, Tensor):
+                    rank_v = torch.flatten(rank_v)
+                    return_data[k] = torch.cat((return_data[k], rank_v))
+        return {k: torch.mean(v) for k, v in return_data.items() if len(v)}
+
+    def to(self, device: Union[str, torch.device] = "cpu") -> None:
+        """Move all metrics to the given device
+
+        Args:
+            device (Union[str, torch.device], optional): Device to move the metrics to. Defaults to "cpu".
+        """
+        for k, v in self._metrics.items():
+            self._metrics[k] = v.to(device)
+
+    def reset(self) -> None:
+        """Reset the internal state of the metrics"""
+        for v in self._metrics.values():
+            v.reset()
 
 
 class MovingAverageMetric(Metric):
