62f52c9fc02ed40caf8d18c2089498c0f6c9442d
rcmalli <refikcan.malli@orobix.com>
2023-09-17 20:38:20 +0000

    feat(RLHF): update data processing pipeline

--- sheeprl/algos/rlhf/data.py
+++ sheeprl/algos/rlhf/data.py
@@ -1,106 +1,219 @@
+import json
+import os
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
 import torch
-from torch.nn.utils.rnn import pad_sequence
+from datasets import load_dataset
+from tqdm import tqdm
+
+from sheeprl.algos.rlhf.utils import prepare_tokenizer
 
 
-class SFTCollate:
-    def __init__(self, dim=1, pad_value=0, ignore_index=-1):
-        self.dim = dim
-        self.pad_value = pad_value
+class DataProcessor:
+    """
+    The main class for processing data for the RLHF algorithm.
+
+    Args:
+        dataset_name (str): The name of the dataset to load.
+        destination_dir (str): The directory where the processed data will be saved.
+        tokenizer_name (str): The name of the tokenizer to use.
+        max_length (int, optional): The maximum length of the input sequences. Defaults to 512.
+        max_prompt_length (int, optional): The maximum length of the prompt sequences. Defaults to 512.
+        num_samples (int, optional): The number of samples to use. Defaults to None.
+        ignore_index (int, optional): The index to use for ignored tokens. Defaults to -1.
+        remove_same_responses (bool, optional): Whether to remove samples with the same response. Defaults to True.
+        remove_same_inputs (bool, optional): Whether to remove samples with the same input. Defaults to True.
+        minimum_response_length (int, optional): The minimum length of the response sequences. Defaults to 2.
+        save_skipped_examples (bool, optional): Whether to save skipped examples. Defaults to False.
+        validation_split (float, optional): The validation split. Defaults to 0.1.
+        reward_model_split (float, optional): The reward model split. Defaults to 0.5.
+        shuffle (bool, optional): Whether to shuffle the dataset. Defaults to True.
+        seed (int, optional): The random seed. Defaults to 42.
+        split_names (List[str], optional): The names of the splits. Defaults to ("train", "val", "test").
+    """
+
+    def __init__(
+        self,
+        dataset_name: str,
+        destination_dir: str,
+        tokenizer_name: str,
+        max_length: int = 512,
+        max_prompt_length: int = 512,
+        num_samples: Optional[int] = None,
+        ignore_index: int = -1,
+        remove_same_responses: bool = True,
+        remove_same_inputs: bool = True,
+        minimum_response_length: int = 2,
+        save_skipped_examples: bool = False,
+        validation_split: float = 0.1,
+        reward_model_split: float = 0.5,
+        shuffle: bool = True,
+        seed: int = 42,
+        split_names: List[str] = ("train", "test"),
+    ):
+        self.dataset_name = dataset_name
+        self.destination_dir = destination_dir
+        self.tokenizer_name = tokenizer_name
+        self.max_length = max_length
+        self.max_prompt_length = max_prompt_length
+        self.num_samples = num_samples
         self.ignore_index = ignore_index
+        self.remove_same_responses = remove_same_responses
+        self.remove_same_inputs = remove_same_inputs
+        self.minimum_response_length = minimum_response_length
+        self.save_skipped_examples = save_skipped_examples
+        self.validation_split = validation_split
+        self.reward_model_split = reward_model_split
+        self.shuffle = shuffle
+        self.seed = seed
+        self.split_names = split_names
+        self.tokenizer = prepare_tokenizer(tokenizer_name)
 
-    def __call__(self, batch):
-        input_ids = [item["input_ids"].type(torch.int64) for item in batch]
-        targets = [item["targets"].type(torch.int64) for item in batch]
+    def process(self):
+        """
+        The main method for processing the data.
+        """
+        destination_dir = Path(self.destination_dir)
+        os.makedirs(destination_dir, exist_ok=True)
+        cache_dir = destination_dir.parent / "cache"
+        bos_token = self.tokenizer.bos_token
+        eos_token = self.tokenizer.eos_token
+        for split in self.split_names:
+            print(f"Processing {split} split ...")
+            dataset = load_dataset(self.dataset_name, split=split, cache_dir=cache_dir)
+            # shuffle the dataset
+            if self.shuffle:
+                dataset = dataset.shuffle(seed=self.seed)
+            if self.num_samples is not None:
+                dataset = dataset.select(range(self.num_samples))
+            samples = []
+            hashes = []
+            skipped_samples = []
+            for sample in tqdm(dataset):
+                output = {}
+                prompt = self.get_prompt(sample)
+                chosen = self.get_chosen(sample)
+                rejected = self.get_rejected(sample)
+                input_hash = hash(prompt + chosen)
+                if self.remove_same_inputs and input_hash in hashes:
+                    skipped_samples.append(
+                        {"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "duplicate input"}
+                    )
+                    continue
+                encoded_prompt = self.tokenizer(
+                    bos_token + prompt,
+                    padding=False,
+                    truncation=True,
+                    max_length=self.max_length,
+                    return_tensors="pt",
+                    add_special_tokens=False,
+                )
+                num_prompt_input_ids = len(encoded_prompt["input_ids"].squeeze())
+                output["prompt_len"] = num_prompt_input_ids
+                if num_prompt_input_ids > self.max_prompt_length:
+                    skipped_samples.append(
+                        {"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "too long"}
+                    )
+                    continue
+                # we need pairs of prompt and choosen and prompt and rejected
+                if len(chosen) < self.minimum_response_length or len(rejected) < self.minimum_response_length:
+                    skipped_samples.append(
+                        {"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "too short"}
+                    )
+                    continue
+                prompt_chosen = bos_token + prompt + chosen + eos_token
+                encoded_prompt_chosen = self.tokenizer(
+                    prompt_chosen,
+                    max_length=self.max_length,
+                    truncation=True,
+                    return_tensors="pt",
+                    add_special_tokens=False,
+                )
 
-        # Use PyTorch's pad_sequence function
-        input_ids = pad_sequence(input_ids, batch_first=True, padding_value=self.pad_value)
-        targets = pad_sequence(targets, batch_first=True, padding_value=self.ignore_index)
-        attention_mask = input_ids.ne(self.pad_value).type(torch.int64)
+                prompt_rejected = bos_token + prompt + rejected + eos_token
+                encoded_prompt_rejected = self.tokenizer(
+                    prompt_rejected,
+                    max_length=self.max_length,
+                    truncation=True,
+                    return_tensors="pt",
+                    add_special_tokens=False,
+                )
+                if self.remove_same_responses and prompt_chosen == prompt_rejected:
+                    skipped_samples.append(
+                        {"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "same response"}
+                    )
+                    continue
+                output["chosen_input_ids"] = encoded_prompt_chosen["input_ids"].squeeze()
+                output["rejected_input_ids"] = encoded_prompt_rejected["input_ids"].squeeze()
+                chosen_masked_targets = output["chosen_input_ids"].clone()
+                chosen_masked_targets[:num_prompt_input_ids] = self.ignore_index
+                output["chosen_masked_targets"] = chosen_masked_targets
+                rejected_masked_targets = output["rejected_input_ids"].clone()
+                rejected_masked_targets[:num_prompt_input_ids] = self.ignore_index
+                output["rejected_masked_targets"] = rejected_masked_targets
+                samples.append(output)
+                hashes.append(input_hash)
+            print(f"Processed {len(samples)} samples, skipped {len(skipped_samples)} samples")
+            output_data = {}
+            if self.reward_model_split > 0:
+                print(f"Using {self.reward_model_split * 100}% of the training data for the reward model training")
+                num_reward_model_samples = int(len(samples) * self.reward_model_split)
+                reward_model_samples = samples[:num_reward_model_samples]
+                finetune_samples = samples[num_reward_model_samples:]
+                output_data["reward_model"] = reward_model_samples
+                output_data["finetune"] = finetune_samples
+            else:
+                output_data["finetune"] = samples
+            if split == "train":
+                print(f"Using {self.validation_split * 100}% of the training data for validation split")
+                for data_name, data in output_data.items():
+                    num_validation_samples = int(len(data) * self.validation_split)
+                    validation_data = data[:num_validation_samples]
+                    train_data = data[num_validation_samples:]
+                    print(
+                        f"Saving {len(train_data)} training samples and"
+                        f"{len(validation_data)} validation samples for {data_name}"
+                    )
+                    torch.save(train_data, destination_dir / f"{data_name}_train.pt")
+                    torch.save(validation_data, destination_dir / f"{data_name}_validation.pt")
+            else:
+                for data_name, data in output_data.items():
+                    print(f"Saving {len(data)} {split} samples for {data_name}")
+                    torch.save(data, destination_dir / f"{data_name}_test.pt")
+            if self.save_skipped_examples:
+                json.dump(skipped_samples, open(destination_dir / f"{split}_skipped.json", "w"), indent=4)
 
-        return {
-            "input_ids": input_ids,
-            "attention_mask": attention_mask,
-            "targets": targets,
+        example_prompt_path = destination_dir / "example_prompt.pt"
+        example_prompt = self.create_example_prompt()
+        torch.save(example_prompt, example_prompt_path)
+
+    def get_prompt(self, sample: Dict[str, Any]) -> str:
+        raise NotImplementedError
+
+    def get_chosen(self, sample: Dict[str, Any]) -> List[str]:
+        raise NotImplementedError
+
+    def get_rejected(self, sample: Dict[str, Any]) -> List[str]:
+        raise NotImplementedError
+
+    def get_example_prompt(self) -> str:
+        raise NotImplementedError
+
+    def wrap_prompt(self, prompt: str) -> str:
+        raise NotImplementedError
+
+    def create_example_prompt(self) -> Dict[str, Any]:
+        prompt = self.get_example_prompt()
+        wrapped_prompt = self.wrap_prompt(prompt)
+
+        wrapped_prompt = self.tokenizer.bos_token + wrapped_prompt
+        encoded_prompt = self.tokenizer(
+            wrapped_prompt, max_length=self.max_length, truncation=True, return_tensors="pt", add_special_tokens=False
+        )
+        output = {
+            "prompt": wrapped_prompt,
+            "input_ids": encoded_prompt["input_ids"],
+            "attention_mask": encoded_prompt["attention_mask"],
         }
-
-
-class RMCollate:
-    def __init__(self, dim=1, pad_value=0, ignore_index=-1):
-        self.dim = dim
-        self.pad_value = pad_value
-        self.ignore_index = ignore_index
-
-    def __call__(self, batch):
-        input_ids = [item["chosen_input_ids"].type(torch.int64) for item in batch]
-        input_ids += [item["rejected_input_ids"].type(torch.int64) for item in batch]
-        # Use PyTorch's pad_sequence function
-        input_ids = pad_sequence(input_ids, batch_first=True, padding_value=self.pad_value)
-        attention_mask = input_ids.ne(self.pad_value).type(torch.int64)
-
-        return {
-            "chosen_input_ids": input_ids[: len(batch)],
-            "rejected_input_ids": input_ids[len(batch) :],
-            "chosen_attention_mask": attention_mask[: len(batch)],
-            "rejected_attention_mask": attention_mask[len(batch) :],
-        }
-
-
-class LeftPadCollate:
-    def __init__(self, dim=1, pad_value=0, ignore_index=-1):
-        self.dim = dim
-        self.pad_value = pad_value
-        self.ignore_index = ignore_index
-
-    def __call__(self, batch):
-        input_ids = [item["input_ids"].type(torch.int64)[: item["prompt_len"]] for item in batch]
-        # Use PyTorch's pad_sequence function
-        # convert into left padding
-        reversed_input_ids = [i.flip(dims=[0]) for i in input_ids]
-        input_ids = pad_sequence(reversed_input_ids, batch_first=True, padding_value=self.pad_value).flip(dims=[1])
-        attention_mask = input_ids.ne(self.pad_value).type(torch.int64)
-
-        return {
-            "prompt_input_ids": input_ids,
-            "prompt_attention_mask": attention_mask,
-        }
-
-
-class EvaluateCollate:
-    def __init__(self, dim=1, pad_value=0, ignore_index=-1):
-        self.dim = dim
-        self.pad_value = pad_value
-        self.ignore_index = ignore_index
-
-    def __call__(self, batch):
-        input_ids_list = []
-        prompt_input_ids_list = []
-        targets_list = []
-        attention_mask = []
-        prompt_len_list = []
-        for item in batch:
-            input_ids = item["input_ids"].type(torch.int64)
-            input_ids_list.append(input_ids)
-            prompt_len = item["prompt_len"]
-            prompt_input_ids_list.append(input_ids[:prompt_len])
-            targets_list.append(item["targets"].type(torch.int64))
-            prompt_len_list.append(prompt_len)
-
-        # Use PyTorch's pad_sequence function
-        padded_input_ids_list = pad_sequence(input_ids_list, batch_first=True, padding_value=self.pad_value)
-        padded_targets_list = pad_sequence(targets_list, batch_first=True, padding_value=self.ignore_index)
-        attention_mask = padded_input_ids_list.ne(self.pad_value).type(torch.int64)
-
-        reversed_input_ids_list = [sample.flip(dims=[0]) for sample in prompt_input_ids_list]
-        padded_reversed_input_ids_list = pad_sequence(
-            reversed_input_ids_list, batch_first=True, padding_value=self.pad_value
-        ).flip(dims=[1])
-        prompt_attention_mask = padded_reversed_input_ids_list.ne(self.pad_value).type(torch.int64)
-
-        return {
-            "input_ids": padded_input_ids_list,
-            "attention_mask": attention_mask,
-            "prompt_input_ids": padded_reversed_input_ids_list,
-            "prompt_attention_mask": prompt_attention_mask,
-            "targets": padded_targets_list,
-            "prompt_len": prompt_len_list,
-        }
+        return output
--- sheeprl/algos/rlhf/helpful_harmless/prepare_data.py
+++ sheeprl/algos/rlhf/helpful_harmless/prepare_data.py
@@ -1,17 +1,11 @@
 import json
-import os
 import sys
 from dataclasses import asdict
 from pathlib import Path
-from typing import Any, Dict, Optional
-
-import torch
-from datasets import load_dataset
-from tqdm import tqdm
-from transformers import PreTrainedTokenizer
+from typing import Any, Dict
 
 from sheeprl.algos.rlhf.args import TextDataArgs
-from sheeprl.algos.rlhf.utils import prepare_tokenizer
+from sheeprl.algos.rlhf.data import DataProcessor
 from sheeprl.utils.parser import HfArgumentParser
 
 # support running without installing as a package
@@ -19,169 +13,31 @@ wd = Path(__file__).parent.parent.resolve()
 sys.path.append(str(wd))
 
 
-def prepare(
-    destination_dir: str,
-    tokenizer_name: str,
-    stage: str = "sft",
-    max_length: int = 512,
-    max_prompt_length: int = 512,
-    mask_prompt: bool = True,
-    num_samples: Optional[int] = None,
-    ignore_index: int = -1,
-    remove_same_responses: bool = True,
-    remove_same_inputs: bool = True,
-    minimum_response_length: int = 2,
-    save_skipped_examples: bool = False,
-    validation_percentage: float = 0.1,
-    shuffle: bool = True,
-    seed: int = 42,
-) -> None:
-    destination_dir = Path(destination_dir)
-    tokenizer = prepare_tokenizer(tokenizer_name)
-    os.makedirs(destination_dir, exist_ok=True)
-    cache_dir = destination_dir.parent / "cache"
-    for split in ["train", "test"]:
-        print(f"Processing {split} split ...")
-        dataset = load_dataset("Dahoas/full-hh-rlhf", split=split, cache_dir=cache_dir)
-        # shuffle the dataset
-        if shuffle:
-            dataset = dataset.shuffle(seed=seed)
-        if stage == "finetune":
-            # first half of the dataset
-            dataset = dataset.select(range(len(dataset) // 2))
-        elif stage == "preference":
-            # second half of the dataset
-            dataset = dataset.select(range(len(dataset) // 2, len(dataset)))
-        else:
-            raise ValueError(f"stage must be one of 'finetune', 'preference', but got {stage}")
+class HHDataProcessor(DataProcessor):
+    def __init__(self, dataset_name: str = "Dahoas/full-hh-rlhf", *args, **kwargs):
+        super().__init__(*args, dataset_name=dataset_name, **kwargs)
 
-        if num_samples is not None:
-            dataset = dataset.select(range(num_samples))
-        samples = []
-        hashes = []
-        skipped_samples = []
-        for sample in tqdm(dataset):
-            output = {}
-            prompt = sample["prompt"]
-            chosen = sample["chosen"]
-            rejected = sample["rejected"]
-            input_hash = hash(prompt + chosen)
-            if remove_same_inputs and input_hash in hashes:
-                skipped_samples.append(
-                    {"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "duplicate input"}
-                )
-                continue
-            encoded_prompt = tokenizer(
-                tokenizer.bos_token + prompt,
-                padding=False,
-                truncation=True,
-                max_length=max_length,
-                return_tensors="pt",
-                add_special_tokens=False,
-            )
-            num_prompt_input_ids = len(encoded_prompt["input_ids"].squeeze())
-            output["prompt_len"] = num_prompt_input_ids
-            if num_prompt_input_ids > max_prompt_length:
-                skipped_samples.append({"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "too long"})
-                continue
-            if stage == "finetune":
-                # we use prompt and choosen as data
-                if len(chosen) < minimum_response_length:
-                    skipped_samples.append(
-                        {"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "too short"}
-                    )
-                    continue
-                prompt_response = tokenizer.bos_token + prompt + chosen + tokenizer.eos_token
-                encoded_prompt_response = tokenizer(
-                    prompt_response,
-                    truncation=True,
-                    max_length=max_length,
-                    return_tensors="pt",
-                    add_special_tokens=False,
-                )
-                input_ids = encoded_prompt_response["input_ids"].squeeze()
-                targets = input_ids.clone()
-                output["input_ids"] = input_ids
-                if mask_prompt:
-                    targets[:num_prompt_input_ids] = ignore_index
-                output["targets"] = targets
-            elif stage == "preference":
-                # we need pairs of prompt and choosen and prompt and rejected
-                chosen = sample["chosen"]
-                rejected = sample["rejected"]
-                if len(chosen) < minimum_response_length or len(rejected) < minimum_response_length:
-                    skipped_samples.append(
-                        {"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "too short"}
-                    )
-                    continue
-                prompt_chosen = tokenizer.bos_token + sample["prompt"] + sample["chosen"] + tokenizer.eos_token
-                encoded_prompt_chosen = tokenizer(
-                    prompt_chosen,
-                    max_length=max_length,
-                    truncation=True,
-                    return_tensors="pt",
-                    add_special_tokens=False,
-                )
+    def get_prompt(self, sample: Dict[str, Any]) -> str:
+        return sample["prompt"]
 
-                prompt_rejected = tokenizer.bos_token + sample["prompt"] + sample["rejected"] + tokenizer.eos_token
-                encoded_prompt_rejected = tokenizer(
-                    prompt_rejected,
-                    max_length=max_length,
-                    truncation=True,
-                    return_tensors="pt",
-                    add_special_tokens=False,
-                )
-                if remove_same_responses and prompt_chosen == prompt_rejected:
-                    skipped_samples.append(
-                        {"prompt": prompt, "chosen": chosen, "rejected": rejected, "reason": "same response"}
-                    )
-                    continue
-                output["chosen_input_ids"] = encoded_prompt_chosen["input_ids"].squeeze()
-                output["rejected_input_ids"] = encoded_prompt_rejected["input_ids"].squeeze()
-            else:
-                raise ValueError(f"stage must be one of 'finetune', 'preference', but got {stage}")
-            samples.append(output)
-            hashes.append(input_hash)
-        print(f"Processed {len(samples)} samples, skipped {len(skipped_samples)} samples")
-        if split == "train":
-            print(f"Using {validation_percentage * 100}% of the training data for validation")
-            num_validation_samples = int(len(samples) * validation_percentage)
-            validation_samples = samples[:num_validation_samples]
-            train_samples = samples[num_validation_samples:]
-            torch.save(train_samples, destination_dir / f"{stage}_train.pt")
-            torch.save(validation_samples, destination_dir / f"{stage}_validation.pt")
-        else:
-            torch.save(samples, destination_dir / f"{stage}_{split}.pt")
-        if save_skipped_examples:
-            json.dump(skipped_samples, open(destination_dir / f"{stage}_{split}_skipped.json", "w"), indent=4)
+    def get_chosen(self, sample: Dict[str, Any]) -> str:
+        return sample["chosen"]
 
-    example_prompt_path = destination_dir / "example_prompt.pt"
-    example_prompt = create_example_prompt(tokenizer, max_length=max_length)
-    torch.save(example_prompt, example_prompt_path)
+    def get_rejected(self, sample: Dict[str, Any]) -> str:
+        return sample["rejected"]
 
+    def wrap_prompt(self, prompt: str) -> str:
+        return "\n\nHuman: " + prompt + "\n\nAssistant: "
 
-def wrap_prompt(prompt: str) -> str:
-    return "\n\nHuman: " + prompt + "\n\nAssistant: "
-
-
-def create_example_prompt(tokenizer: PreTrainedTokenizer, max_length: int) -> Dict[str, Any]:
-    prompt = "How does the computer work?"
-    wrapped_prompt = tokenizer.bos_token + wrap_prompt(prompt)
-    encoded_prompt = tokenizer(
-        wrapped_prompt, max_length=max_length, truncation=True, add_special_tokens=False, return_tensors="pt"
-    )
-    output = {
-        "prompt": wrapped_prompt,
-        "input_ids": encoded_prompt["input_ids"],
-        "attention_mask": encoded_prompt["attention_mask"],
-    }
-    return output
+    def get_example_prompt(self) -> str:
+        return "How does the computer work?"
 
 
 if __name__ == "__main__":
     parser = HfArgumentParser([TextDataArgs])
     dataclasses = parser.parse_args_into_dataclasses()
     data_args: TextDataArgs = dataclasses[0]
-    prepare(**asdict(data_args))
+    data_processor = HHDataProcessor(**asdict(data_args))
+    data_processor.process()
     with open(Path(data_args.destination_dir) / "args.json", "w") as f:
         json.dump(asdict(data_args), f, indent=4)
