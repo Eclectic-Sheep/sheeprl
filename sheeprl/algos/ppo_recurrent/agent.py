1ecb138da0bcd1bdd2e51951e7892895a681effa
Michele Milesi <michele.milesi@studio.unibo.it>
2023-09-21 12:04:02 +0200

    feat: modified recurrent ppo agent

--- sheeprl/algos/ppo_recurrent/agent.py
+++ sheeprl/algos/ppo_recurrent/agent.py
@@ -1,149 +1,216 @@
-from typing import Optional, Tuple
+from math import prod
+from typing import Any, Dict, List, Sequence, Tuple, Union
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from omegaconf import DictConfig
 from torch import Tensor
+from torch.distributions import Independent, Normal, OneHotCategorical
 
-from sheeprl.models.models import MLP
+from sheeprl.algos.dreamer_v3.agent import LayerNormGRUCell
+from sheeprl.algos.ppo.agent import CNNEncoder, MLPEncoder
+from sheeprl.models.models import MLP, MultiEncoder
+
+
+class RecurrentModel(nn.Module):
+    def __init__(
+        self,
+        input_size: int,
+        gru_hidden_size: int,
+        mlp_dense_units: int,
+        mlp_activation: nn.Module,
+        gru_bias: bool,
+        mlp_bias: bool,
+        gru_layer_norm: bool,
+        mlp_layer_norm: bool,
+        mlp_pre_rnn: bool = True,
+    ) -> None:
+        super().__init__()
+        self._mlp_pre_rnn = mlp_pre_rnn
+        if mlp_pre_rnn:
+            self._mlp = MLP(
+                input_dims=input_size,
+                output_dim=None,
+                hidden_sizes=[mlp_dense_units],
+                activation=mlp_activation,
+                layer_args={"bias": mlp_bias},
+                norm_layer=[nn.LayerNorm] if mlp_layer_norm else None,
+                norm_args=[{"normalized_shape": mlp_dense_units, "eps": 1e-3}] if mlp_layer_norm else None,
+            )
+        self._gru = LayerNormGRUCell(
+            input_size=mlp_dense_units if mlp_pre_rnn else input_size,
+            hidden_size=gru_hidden_size,
+            bias=gru_bias,
+            batch_first=False,
+            layer_norm=gru_layer_norm,
+        )
+
+    def forward(self, input: Tensor, hx: Tensor) -> Tensor:
+        feat = self._mlp(input) if self._mlp_pre_rnn else input
+        out = self._gru(feat, hx)
+        return out
 
 
 class RecurrentPPOAgent(nn.Module):
     def __init__(
         self,
-        observation_dim: int,
-        action_dim: int,
-        lstm_hidden_size: int = 64,
-        actor_hidden_size: int = 128,
-        actor_pre_lstm_hidden_size: Optional[int] = None,
-        critic_hidden_size: int = 128,
-        critic_pre_lstm_hidden_size: Optional[int] = None,
+        actions_dim: int,
+        obs_space: Dict[str, Any],
+        encoder_cfg: DictConfig,
+        rnn_cfg: DictConfig,
+        actor_cfg: DictConfig,
+        critic_cfg: DictConfig,
+        cnn_keys: Sequence[str],
+        mlp_keys: Sequence[str],
+        is_continuous: bool,
         num_envs: int = 1,
+        screen_size: int = 64,
+        device: Union[torch.device, str] = "cpu",
     ):
         super().__init__()
-        self.observation_dim = observation_dim
-        self.action_dim = action_dim
-        self.lstm_hidden_size = lstm_hidden_size
+        self.device = torch.device(device) if isinstance(device, str) else device
+
+        self.actions_dim = actions_dim
+        in_channels = sum([prod(obs_space[k].shape[:-2]) for k in cnn_keys])
+        mlp_input_dim = sum([obs_space[k].shape[0] for k in mlp_keys])
+        cnn_encoder = (
+            CNNEncoder(in_channels, encoder_cfg.cnn_features_dim, screen_size, cnn_keys)
+            if cnn_keys is not None and len(cnn_keys) > 0
+            else None
+        )
+        mlp_encoder = (
+            MLPEncoder(
+                mlp_input_dim,
+                encoder_cfg.mlp_features_dim,
+                mlp_keys,
+                encoder_cfg.dense_units,
+                encoder_cfg.mlp_layers,
+                eval(encoder_cfg.dense_act),
+                encoder_cfg.layer_norm,
+            )
+            if mlp_keys is not None and len(mlp_keys) > 0
+            else None
+        )
+        self.feature_extractor = MultiEncoder(cnn_encoder, mlp_encoder)
+        self.is_continuous = is_continuous
+        features_dim = self.feature_extractor.output_dim
+
+        self.rnn = RecurrentModel(
+            input_size=features_dim + actions_dim,
+            gru_hidden_size=rnn_cfg.gru.hidden_size,
+            mlp_dense_units=rnn_cfg.mlp.dense_units,
+            mlp_activation=eval(rnn_cfg.mlp.activation),
+            gru_bias=rnn_cfg.gru.bias,
+            mlp_bias=rnn_cfg.mlp.bias,
+            gru_layer_norm=rnn_cfg.gru.layer_norm,
+            mlp_layer_norm=rnn_cfg.mlp.layer_norm,
+            mlp_pre_rnn=rnn_cfg.mlp_pre_rnn,
+        )
+
+        self.critic = MLP(
+            input_dims=features_dim,
+            output_dim=1,
+            hidden_sizes=[critic_cfg.dense_units] * critic_cfg.mlp_layers,
+            activation=eval(critic_cfg.dense_act),
+            norm_layer=[nn.LayerNorm for _ in range(critic_cfg.mlp_layers)] if critic_cfg.layer_norm else None,
+            norm_args=(
+                [{"normalized_shape": critic_cfg.dense_units} for _ in range(critic_cfg.mlp_layers)]
+                if critic_cfg.layer_norm
+                else None
+            ),
+        )
+        self.actor_backbone = MLP(
+            input_dims=features_dim,
+            output_dim=None,
+            hidden_sizes=[actor_cfg.dense_units] * actor_cfg.mlp_layers,
+            activation=eval(actor_cfg.dense_act),
+            flatten_dim=None,
+            norm_layer=[nn.LayerNorm] * actor_cfg.mlp_layers if actor_cfg.layer_norm else None,
+            norm_args=(
+                [{"normalized_shape": actor_cfg.dense_units} for _ in range(actor_cfg.mlp_layers)]
+                if actor_cfg.layer_norm
+                else None
+            ),
+        )
+        if is_continuous:
+            self.actor_heads = nn.ModuleList([nn.Linear(actor_cfg.dense_units, sum(actions_dim) * 2)])
+        else:
+            self.actor_heads = nn.ModuleList(
+                [nn.Linear(actor_cfg.dense_units, action_dim) for action_dim in actions_dim]
+            )
+
+        self.actions_dim = actions_dim
+        self.rnn_hidden_size = rnn_cfg.gru.hidden_size
         self.num_envs = num_envs
 
-        # Actor: Obs -> Feature -> LSTM -> Logits
-        if actor_pre_lstm_hidden_size is None:
-            self._actor_fc = nn.Flatten(start_dim=2)
-            lstm_input_size = observation_dim
-        else:
-            self._actor_fc = MLP(
-                input_dims=observation_dim,
-                output_dim=lstm_hidden_size,
-                hidden_sizes=(actor_pre_lstm_hidden_size,),
-                flatten_dim=2,
-            )
-            lstm_input_size = self._actor_fc.output_dim
-        self._actor_rnn = nn.LSTM(input_size=lstm_input_size, hidden_size=lstm_hidden_size, batch_first=False)
-        self._actor_logits = MLP(lstm_hidden_size, action_dim, (actor_hidden_size, actor_hidden_size), flatten_dim=None)
-
-        # Critic: Obs -> Feature -> LSTM -> Values
-        if critic_pre_lstm_hidden_size is None:
-            self._critic_fc = nn.Flatten(start_dim=2)
-            lstm_input_size = observation_dim
-        else:
-            self._critic_fc = MLP(
-                input_dims=observation_dim,
-                output_dim=lstm_hidden_size,
-                hidden_sizes=(critic_pre_lstm_hidden_size,),
-                flatten_dim=2,
-            )
-            lstm_input_size = self._critic_fc.output_dim
-        self._critic_rnn = nn.LSTM(input_size=lstm_input_size, hidden_size=lstm_hidden_size, batch_first=False)
-        self._critic = MLP(lstm_hidden_size, 1, (critic_hidden_size, critic_hidden_size), flatten_dim=None)
-
         # Initial recurrent states for both the actor and critic rnn
-        self._initial_states: Tuple[Tuple[Tensor, Tensor], Tuple[Tensor, Tensor]] = self.reset_hidden_states()
+        self._initial_states: Tensor = self.reset_hidden_states()
 
     @property
-    def initial_states(self) -> Tuple[Tuple[Tensor, Tensor], Tuple[Tensor, Tensor]]:
+    def initial_states(self) -> Tensor:
         return self._initial_states
 
     @initial_states.setter
-    def initial_states(self, value: Tuple[Tuple[Tensor, Tensor], Tuple[Tensor, Tensor]]) -> None:
+    def initial_states(self, value: Tensor) -> None:
         self._initial_states = value
 
-    def reset_hidden_states(self) -> Tuple[Tuple[Tensor, Tensor], Tuple[Tensor, Tensor]]:
-        actor_state = (
-            torch.zeros(1, self.num_envs, self.lstm_hidden_size),
-            torch.zeros(1, self.num_envs, self.lstm_hidden_size),
-        )
-        critic_state = (
-            torch.zeros(1, self.num_envs, self.lstm_hidden_size),
-            torch.zeros(1, self.num_envs, self.lstm_hidden_size),
-        )
-        return (actor_state, critic_state)
+    def reset_hidden_states(self) -> Tensor:
+        hx = torch.zeros(1, self.num_envs, self.rnn_hidden_size, device=self.device)
+        return hx
 
-    def get_greedy_action(self, obs: Tensor, state: Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tuple[Tensor, Tensor]]:
-        x = self._actor_fc(obs)
-        x, state = self._actor_rnn(x, state)
-        logits = self._actor_logits(x)
-        probs = F.softmax(logits, dim=-1)
-        return torch.argmax(probs, dim=-1), state
+    def get_greedy_actions(self, pre_dist: Tuple[Tensor, ...]) -> Tuple[Tensor, ...]:
+        actions = []
+        if self.is_continuous:
+            dist = Independent(Normal(*pre_dist), 1)
+            actions.append(dist.mode)
+        else:
+            for logits in pre_dist:
+                dist = OneHotCategorical(logits=logits)
+                actions.append(dist.mode)
+        return tuple(actions)
 
-    def get_logits(
-        self, obs: Tensor, actor_state: Tuple[Tensor, Tensor], mask: Optional[Tensor] = None
-    ) -> Tuple[Tensor, Tuple[Tensor, Tensor]]:
-        x_actor = self._actor_fc(obs)
-        self._actor_rnn.flatten_parameters()
-        if mask is not None:
-            lengths = mask.sum(dim=0).view(-1).cpu()
-            x_actor = torch.nn.utils.rnn.pack_padded_sequence(
-                x_actor, lengths=lengths, batch_first=False, enforce_sorted=False
-            )
-        actor_hidden, actor_state = self._actor_rnn(x_actor, actor_state)
-        if mask is not None:
-            actor_hidden, _ = torch.nn.utils.rnn.pad_packed_sequence(
-                actor_hidden, batch_first=False, total_length=obs.shape[0]
-            )
-        logits = self._actor_logits(actor_hidden)
-        return logits, actor_state
+    def get_sampled_actions(self, pre_dist: Tuple[Tensor, ...]) -> Tuple[Tuple[Tensor, ...], Tensor]:
+        actions = []
+        logprobs = []
+        if self.is_continuous:
+            dist = Independent(Normal(*pre_dist), 1)
+            actions.append(dist.sample())
+            logprobs.append(dist.log_prob(actions))
+        else:
+            for logits in pre_dist:
+                dist = OneHotCategorical(logits=logits)
+                actions.append(dist.sample())
+                logprobs.append(dist.log_prob(actions[-1]))
+        return tuple(actions), torch.stack(logprobs, dim=-1).sum(dim=-1)
 
-    def get_values(
-        self, obs: Tensor, critic_state: Tuple[Tensor, Tensor], mask: Optional[Tensor] = None
-    ) -> Tuple[Tensor, Tuple[Tensor, Tensor]]:
-        x_critic = self._critic_fc(obs)
-        self._critic_rnn.flatten_parameters()
-        if mask is not None:
-            lengths = mask.sum(dim=0).view(-1).cpu()
-            x_critic = torch.nn.utils.rnn.pack_padded_sequence(
-                x_critic, lengths=lengths, batch_first=False, enforce_sorted=False
-            )
-        critic_hidden, critic_state = self._critic_rnn(x_critic, critic_state)
-        if mask is not None:
-            critic_hidden, _ = torch.nn.utils.rnn.pad_packed_sequence(
-                critic_hidden, batch_first=False, total_length=obs.shape[0]
-            )
-        values = self._critic(critic_hidden)
-        return values, critic_state
+    def get_pre_dist(self, hx: Tensor) -> Union[Tuple[Tensor, ...], Tuple[Tensor, Tensor]]:
+        actor_features = self.actor_backbone(hx)
+        pre_dist: List[Tensor] = [head(actor_features) for head in self.actor_heads]
+        if self.is_continuous:
+            mean, log_std = torch.chunk(pre_dist[0], chunks=2, dim=-1)
+            std = log_std.exp()
+            return (mean, std)
+        else:
+            return tuple(pre_dist)
 
-    def forward(
-        self,
-        obs: Tensor,
-        state: Tuple[Tuple[Tensor, Tensor], Tuple[Tensor, Tensor]] = ((None, None), (None, None)),
-        mask: Optional[Tensor] = None,
-    ) -> Tuple[Tensor, Tensor, Tuple[Tuple[Tensor, Tensor], Tuple[Tensor, Tensor]]]:
+    def get_values(self, hx: Tensor) -> Tensor:
+        return self.critic(hx)
+
+    def forward(self, obs: Dict[str, Tensor], prev_actions: Tensor, prev_hx: Tensor) -> Tuple[Tensor, Tensor]:
         """Compute actor logits and critic values.
 
         Args:
             obs (Tensor): observations collected (possibly padded with zeros).
-            state (Tuple[Tuple[Tensor, Tensor], Tuple[Tensor, Tensor]], optional): the recurrent states.
-                Defaults to None.
-            mask (Tensor, optional): boolean mask with valid indices. This is used to pack the padded
-                sequences during training. If None, no packing will be done.
-                Defaults to None.
+            prev_actions (Tensor): the previous actions.
+            prev_hx (Tensor): the previous state of the GRU.
 
         Returns:
             actor logits
             critic values
-            next recurrent state for both the actor and the critic
+            the next state of the GRU.
         """
-        device = obs.device
-        actor_state, critic_state = state
-        logits, actor_state = self.get_logits(obs, tuple([s.to(device) for s in actor_state]), mask)
-        values, critic_state = self.get_values(obs, tuple([s.to(device) for s in critic_state]), mask)
-        return logits, values, (actor_state, critic_state)
+        embedded_obs = self.feature_extractor(obs)
+        hx = self.rnn(torch.cat((embedded_obs, prev_actions), dim=-1), prev_hx)
+        pre_dist = self.get_pre_dist(hx)
+        actions = self.get_sampled_actions(pre_dist)
+        return actions, hx
