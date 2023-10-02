c4e9bf39f04ddb48edec147389d1810f6cb3a3b4
michele-milesi <74559684+michele-milesi@users.noreply.github.com>
2023-10-02 13:06:44 +0200

    Feature/update diambra (#111)
    * update diambra-arena (v2.2.1)
    
    * fix: diambra wrapper
    
    * docs: update diambra howto

--- sheeprl/envs/diambra.py
+++ sheeprl/envs/diambra.py
@@ -11,9 +11,9 @@ from typing import Any, Dict, List, Optional, SupportsFloat, Tuple, Union
 
 import diambra
 import diambra.arena
-import gym
-import gymnasium
+import gymnasium as gym
 import numpy as np
+from diambra.arena import EnvironmentSettings, WrappersSettings
 from gymnasium import core
 from gymnasium.core import RenderFrame
 
@@ -25,12 +25,12 @@ class DiambraWrapper(core.Env):
         action_space: str = "discrete",
         screen_size: Union[int, Tuple[int, int]] = 64,
         grayscale: bool = False,
-        attack_but_combination: bool = True,
-        sticky_actions: int = 1,
-        seed: Optional[int] = None,
+        repeat_action: int = 1,
         rank: int = 0,
         diambra_settings: Dict[str, Any] = {},
         diambra_wrappers: Dict[str, Any] = {},
+        render_mode: str = "rgb_array",
+        log_level: int = 0,
     ) -> None:
         super().__init__()
 
@@ -39,45 +39,49 @@ class DiambraWrapper(core.Env):
 
         if diambra_settings.pop("frame_shape", None) is not None:
             warnings.warn("The DIAMBRA frame_shape setting is disabled")
-        settings = {
+        if diambra_settings.pop("n_players", None) is not None:
+            warnings.warn("The DIAMBRA n_players setting is disabled")
+
+        role = diambra_settings.pop("role", None)
+        settings = EnvironmentSettings(
             **diambra_settings,
-            "action_space": action_space.lower(),
-            "attack_but_combination": attack_but_combination,
-        }
-        if sticky_actions > 1:
+            **{
+                "game_id": id,
+                "action_space": eval(action_space),
+                "n_players": 1,
+                "role": eval(role) if role is not None else None,
+                "render_mode": render_mode,
+            },
+        )
+        if repeat_action > 1:
             if "step_ratio" not in settings or settings["step_ratio"] > 1:
                 warnings.warn(
-                    f"step_ratio parameter modified to 1 because the sticky action is active ({sticky_actions})"
+                    f"step_ratio parameter modified to 1 because the sticky action is active ({repeat_action})"
                 )
             settings["step_ratio"] = 1
-        if diambra_wrappers.pop("hwc_obs_resize", None) is not None:
-            warnings.warn("The DIAMBRA hwc_obs_resize wrapper is disabled")
-        if diambra_wrappers.pop("frame_stack", None) is not None:
-            warnings.warn("The DIAMBRA frame_stack wrapper is disabled")
+        if diambra_wrappers.pop("frame_shape", None) is not None:
+            warnings.warn("The DIAMBRA frame_shape wrapper is disabled")
+        if diambra_wrappers.pop("stack_frames", None) is not None:
+            warnings.warn("The DIAMBRA stack_frames wrapper is disabled")
         if diambra_wrappers.pop("dilation", None) is not None:
             warnings.warn("The DIAMBRA dilation wrapper is disabled")
-        wrappers = {
+        if diambra_wrappers.pop("flatten", None) is not None:
+            warnings.warn("The DIAMBRA flatten wrapper is disabled")
+        wrappers = WrappersSettings(
             **diambra_wrappers,
-            "flatten": True,
-            "sticky_actions": sticky_actions,
-            "hwc_obs_resize": screen_size + (1 if grayscale else 3,),
-        }
-        self._env = diambra.arena.make(id, settings, wrappers, seed=seed, rank=rank)
+            **{
+                "flatten": True,
+                "repeat_action": repeat_action,
+                "frame_shape": screen_size + (int(grayscale),),
+            },
+        )
+        self._env = diambra.arena.make(id, settings, wrappers, rank=rank, render_mode=render_mode, log_level=log_level)
 
         # Observation and action space
-        self.action_space = (
-            gymnasium.spaces.Discrete(self._env.action_space.n)
-            if action_space.lower() == "discrete"
-            else gymnasium.spaces.MultiDiscrete(self._env.action_space.nvec)
-        )
+        self.action_space = self._env.action_space
         obs = {}
         for k in self._env.observation_space.spaces.keys():
-            if isinstance(self._env.observation_space[k], gym.spaces.Box):
-                low = self._env.observation_space[k].low
-                high = self._env.observation_space[k].high
-                shape = self._env.observation_space[k].shape
-                dtype = self._env.observation_space[k].dtype
-            elif isinstance(self._env.observation_space[k], gym.spaces.Discrete):
+            if isinstance(self._env.observation_space[k], gym.spaces.Discrete):
                 low = 0
                 high = self._env.observation_space[k].n - 1
                 shape = (1,)
@@ -87,11 +91,15 @@ class DiambraWrapper(core.Env):
                 high = self._env.observation_space[k].nvec - 1
                 shape = (len(high),)
                 dtype = np.int32
-            else:
+            elif not isinstance(self._env.observation_space[k], gym.spaces.Box):
                 raise RuntimeError(f"Invalid observation space, got: {type(self._env.observation_space[k])}")
-            obs[k] = gymnasium.spaces.Box(low, high, shape, dtype)
-        self.observation_space = gymnasium.spaces.Dict(obs)
-        self.render_mode = "rgb_array"
+            obs[k] = (
+                self._env.observation_space[k]
+                if isinstance(self._env.observation_space[k], gym.spaces.Box)
+                else gym.spaces.Box(low, high, shape, dtype)
+            )
+        self.observation_space = gym.spaces.Dict(obs)
+        self.render_mode = render_mode
 
     def __getattr__(self, name):
         return getattr(self._env, name)
@@ -103,17 +111,19 @@ class DiambraWrapper(core.Env):
         }
 
     def step(self, action: Any) -> Tuple[Any, SupportsFloat, bool, bool, Dict[str, Any]]:
-        obs, reward, done, infos = self._env.step(action)
+        obs, reward, done, truncated, infos = self._env.step(action)
         infos["env_domain"] = "DIAMBRA"
-        return self._convert_obs(obs), reward, done or infos.get("env_done", False), False, infos
+        return self._convert_obs(obs), reward, done or infos.get("env_done", False), truncated, infos
 
     def render(self, mode: str = "rgb_array", **kwargs) -> Optional[Union[RenderFrame, List[RenderFrame]]]:
-        return self._env.render("rgb_array")
+        return self._env.render()
 
     def reset(
         self, *, seed: Optional[int] = None, options: Optional[Dict[str, Any]] = None
     ) -> Tuple[Any, Dict[str, Any]]:
-        return self._convert_obs(self._env.reset()), {"env_domain": "DIAMBRA"}
+        obs, infos = self._env.reset(seed=seed, options=options)
+        infos["env_domain"] = "DIAMBRA"
+        return self._convert_obs(obs), infos
 
     def close(self) -> None:
         self._env.close()
