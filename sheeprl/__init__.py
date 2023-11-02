1d5cab1fb81bb34ea10180f0a78eede776498941
Michele Milesi <michele.milesi@studio.unibo.it>
2023-11-02 09:37:09 +0100

    fix: evaluate and __init__

--- sheeprl/__init__.py
+++ sheeprl/__init__.py
@@ -20,6 +20,8 @@ from sheeprl.algos.dreamer_v3 import dreamer_v3  # noqa: F401
 from sheeprl.algos.droq import droq  # noqa: F401
 from sheeprl.algos.p2e_dv1 import p2e_dv1  # noqa: F401
 from sheeprl.algos.p2e_dv2 import p2e_dv2  # noqa: F401
+from sheeprl.algos.p2e_dv3 import p2e_dv3_exploration  # noqa: F401
+from sheeprl.algos.p2e_dv3 import p2e_dv3_finetuning  # noqa: F401
 from sheeprl.algos.ppo import ppo  # noqa: F401
 from sheeprl.algos.ppo import ppo_decoupled  # noqa: F401
 from sheeprl.algos.ppo_recurrent import ppo_recurrent  # noqa: F401
@@ -33,6 +35,7 @@ from sheeprl.algos.dreamer_v3 import evaluate as dreamer_v3_evaluate  # noqa: F4
 from sheeprl.algos.droq import evaluate as droq_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.p2e_dv1 import evaluate as p2e_dv1_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.p2e_dv2 import evaluate as p2e_dv2_evaluate  # noqa: F401, isort:skip
+from sheeprl.algos.p2e_dv3 import evaluate as p2e_dv3_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.ppo import evaluate as ppo_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.ppo_recurrent import evaluate as ppo_recurrent_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.sac import evaluate as sac_evaluate  # noqa: F401, isort:skip
--- sheeprl/algos/p2e_dv3/evaluate.py
+++ sheeprl/algos/p2e_dv3/evaluate.py
@@ -13,7 +13,7 @@ from sheeprl.utils.logger import create_tensorboard_logger, get_log_dir
 from sheeprl.utils.registry import register_evaluation
 
 
-@register_evaluation(algorithms="p2e_dv3")
+@register_evaluation(algorithms=["p2e_dv3_exploration", "p2e_dv3_finetuning"])
 def evaluate(fabric: Fabric, cfg: Dict[str, Any], state: Dict[str, Any]):
     logger = create_tensorboard_logger(fabric, cfg)
     if logger and fabric.is_global_zero:
