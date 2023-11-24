fd8f6d0a47bd1787050da985dc5a4e6ce33880f1
belerico_t <federico.belotti@orobix.com>
2023-11-24 09:20:06 +0100

    Update tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -3,4 +3,4 @@ import sys
 import pytest
 
 if __name__ == "__main__":
-    sys.exit(pytest.main(["-s", "--cov=sheeprl", "-vv", "tests/test_algos/test_cli.py"]))
+    sys.exit(pytest.main(["-s", "--cov=sheeprl", "-vv"]))
