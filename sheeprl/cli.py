12f754c4ea9f94e1253ce055cd7876740c08f7fa
belerico <belo.fede@outlook.com>
2024-01-31 08:50:07 +0100

    Set seed before the evaluation starts

--- sheeprl/cli.py
+++ sheeprl/cli.py
@@ -177,6 +177,9 @@ def eval_algorithm(cfg: DictConfig):
         cfg.fabric, accelerator=accelerator, devices=1, num_nodes=1, _convert_="all"
     )
 
+    # Seed everything
+    fabric.seed_everything(cfg.seed)
+
     # Load the checkpoint
     state = fabric.load(cfg.checkpoint_path)
 
@@ -300,10 +303,11 @@ def evaluation(cfg: DictConfig):
     # Load the checkpoint configuration
     checkpoint_path = Path(cfg.checkpoint_path)
     ckpt_cfg = OmegaConf.load(checkpoint_path.parent.parent / "config.yaml")
+    ckpt_cfg.pop("seed", None)
 
     # Merge the two configs
     with open_dict(cfg):
-        capture_video = getattr(cfg.env, "capture_video", True)
+        capture_video = getattr(cfg.env, "capture_video", False)
         cfg.env = {"capture_video": capture_video, "num_envs": 1}
         cfg.exp = {}
         cfg.algo = {}
