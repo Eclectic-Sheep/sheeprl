fix: observation normalization  (#214)
* fix: added -0.5 before cnn forward to bring observations into the range [-0.5, 0.5].

* Fix normalization for DV3 agent

* Explicit normalization applied outside the agent

* feat: remove + 0.5 in decoder output for dv3 and p2e_dv3

---------

Co-authored-by: belerico <belo.fede@outlook.com>
Co-authored-by: Federico Belotti <federico.belotti@orobix.com>

sheeprl/algos/dreamer_v3/dreamer_v3.py
@@ -1,6 +1,7 @@
 """Dreamer-V3 implementation from [https://arxiv.org/abs/2301.04104](https://arxiv.org/abs/2301.04104)
 Adapted from the original implementation from https://github.com/danijar/dreamerv3
 """
+
 from __future__ import annotations
 
 import copy
@@ -96,7 +97,7 @@ def train(
     stochastic_size = cfg.algo.world_model.stochastic_size
     discrete_size = cfg.algo.world_model.discrete_size
     device = fabric.device
-    batch_obs = {k: data[k] / 255.0 for k in cfg.algo.cnn_keys.encoder}
+    batch_obs = {k: data[k] / 255.0 - 0.5 for k in cfg.algo.cnn_keys.encoder}
     batch_obs.update({k: data[k] for k in cfg.algo.mlp_keys.encoder})
     data["is_first"][0, :] = torch.ones_like(data["is_first"][0, :])
 
@@ -590,7 +591,7 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
                     for k, v in obs.items():
                         preprocessed_obs[k] = torch.as_tensor(v[np.newaxis], dtype=torch.float32, device=device)
                         if k in cfg.algo.cnn_keys.encoder:
-                            preprocessed_obs[k] = preprocessed_obs[k] / 255.0
+                            preprocessed_obs[k] = preprocessed_obs[k] / 255.0 - 0.5
                     mask = {k: v for k, v in preprocessed_obs.items() if k.startswith("mask")}
                     if len(mask) == 0:
                         mask = None