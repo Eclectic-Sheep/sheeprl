Fix/player precision plugin (#244)
* Fix PPO player with precision

* Fix PPO agent to run with the correct precision plugin

* Add detach_actor + fix creation of testing agent

* FIx SAC to use the correct precision plugin

* Add get_single_devie_fabric method

* Fix DrOQ agent to handle fp16

* Fix SACAE agent to handle fp16

* Fix PPO recurrent to handle fp16

* Fix Join context

* Fix PPO evaluate

* Fix Dreamer-V1 player to handle fp16

* Dreamer-V1 inference mode

* Fix Dreamer-V2 to handle fp16

* Fix Dreamer-V3 to handle fp16

* Extract module inside the player

* Fix P2E-DV1 to handle fp16

* Fix p2e-dv2 to handle fp16

* Fix p2e-dv3 to handle fp16

* Fix A2C to handle fp16

* Add `greedy` flag to actor forward method

* Fix PPORecurrent arg postion

* Let P2E algos handle fp16

* Wrap target critics with a single-device fabric

* import annotations from future

* Fix P2E actor task during test

* Wrap actor with single-device fabric

sheeprl/algos/dreamer_v1/agent.py
@@ -17,6 +17,7 @@ from sheeprl.algos.dreamer_v2.agent import CNNDecoder, CNNEncoder
 from sheeprl.algos.dreamer_v2.agent import MinedojoActor as DV2MinedojoActor
 from sheeprl.algos.dreamer_v2.agent import MLPDecoder, MLPEncoder
 from sheeprl.models.models import MLP, MultiDecoder, MultiEncoder
+from sheeprl.utils.fabric import get_single_device_fabric
 from sheeprl.utils.utils import init_weights
 
 # In order to use the hydra.utils.get_class method, in this way the user can
@@ -221,45 +222,54 @@ class PlayerDV1(nn.Module):
     """The model of the DreamerV1 player.
 
     Args:
-        encoder (nn.Module): the encoder.
-        recurrent_model (nn.Module): the recurrent model.
-        representation_model (nn.Module): the representation model.
-        actor (nn.Module): the actor.
+        fabric (Fabric): the fabric object.
+        encoder (nn.Module| _FabricModule): the encoder.
+        recurrent_model (nn.Module| _FabricModule): the recurrent model.
+        representation_model (nn.Module| _FabricModule): the representation model.
+        actor (nn.Module| _FabricModule): the actor.
         actions_dim (Sequence[int]): the dimension of each action.
         num_envs (int): the number of environments.
         stochastic_size (int): the size of the stochastic state.
         recurrent_state_size (int): the size of the recurrent state.
-        device (torch.device): the device to work on.
         actor_type (str, optional): which actor the player is using ('task' or 'exploration').
             Default to None.
     """
 
     def __init__(
         self,
-        encoder: nn.Module,
-        recurrent_model: nn.Module,
-        representation_model: nn.Module,
-        actor: nn.Module,
+        fabric: Fabric,
+        encoder: nn.Module | _FabricModule,
+        recurrent_model: nn.Module | _FabricModule,
+        representation_model: nn.Module | _FabricModule,
+        actor: nn.Module | _FabricModule,
         actions_dim: Sequence[int],
         num_envs: int,
         stochastic_size: int,
         recurrent_state_size: int,
-        device: torch.device,
         actor_type: str | None = None,
     ) -> None:
         super().__init__()
-        self.encoder = encoder
-        self.recurrent_model = recurrent_model
-        self.representation_model = representation_model
-        self.actor = actor
-        self.device = device
+        single_device_fabric = get_single_device_fabric(fabric)
+        self.encoder = single_device_fabric.setup_module(
+            getattr(encoder, "module", encoder),
+        )
+        self.recurrent_model = single_device_fabric.setup_module(
+            getattr(recurrent_model, "module", recurrent_model),
+        )
+        self.representation_model = single_device_fabric.setup_module(
+            getattr(representation_model, "module", representation_model)
+        )
+        self.actor = single_device_fabric.setup_module(
+            getattr(actor, "module", actor),
+        )
+        self.device = single_device_fabric.device
         self.actions_dim = actions_dim
         self.stochastic_size = stochastic_size
         self.recurrent_state_size = recurrent_state_size
         self.num_envs = num_envs
         self.validate_args = self.actor.distribution_cfg.validate_args
-        self.init_states()
         self.actor_type = actor_type
+        self.init_states()
 
     def init_states(self, reset_envs: Optional[Sequence[int]] = None) -> None:
         """Initialize the states and the actions for the ended environments.

sheeprl/algos/droq/droq.py
@@ -21,6 +21,7 @@ from sheeprl.algos.sac.loss import entropy_loss, policy_loss
 from sheeprl.algos.sac.sac import test
 from sheeprl.data.buffers import ReplayBuffer
 from sheeprl.utils.env import make_env
+from sheeprl.utils.fabric import get_single_device_fabric
 from sheeprl.utils.logger import get_log_dir, get_logger
 from sheeprl.utils.metric import MetricAggregator
 from sheeprl.utils.registry import register_algorithm
@@ -194,6 +195,8 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
     agent = build_agent(
         fabric, cfg, observation_space, action_space, state["agent"] if cfg.checkpoint.resume_from else None
     )
+    fabric_player = get_single_device_fabric(fabric)
+    actor = fabric_player.setup_module(agent.actor.module)
 
     # Optimizers
     qf_optimizer = hydra.utils.instantiate(cfg.algo.critic.optimizer, params=agent.qfs.parameters(), _convert_="all")
@@ -285,7 +288,7 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
         with timer("Time/env_interaction_time", SumMetric, sync_on_compute=False):
             with torch.no_grad():
                 # Sample an action given the observation received by the environment
-                actions, _ = agent.actor.module(torch.from_numpy(obs).to(device))
+                actions, _ = actor(torch.from_numpy(obs).to(device))
                 actions = actions.cpu().numpy()
             next_obs, rewards, dones, truncated, infos = envs.step(actions.reshape(envs.action_space.shape))
             dones = np.logical_or(dones, truncated)
@@ -385,7 +388,7 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
 
     envs.close()
     if fabric.is_global_zero and cfg.algo.run_test:
-        test(agent.actor.module, fabric, cfg, log_dir)
+        test(actor, fabric, cfg, log_dir)
 
     if not cfg.model_manager.disabled and fabric.is_global_zero:
         from sheeprl.algos.sac.utils import log_models

sheeprl/algos/p2e_dv3/p2e_dv3_finetuning.py
@@ -17,6 +17,7 @@ from sheeprl.algos.dreamer_v3.utils import Moments, test
 from sheeprl.algos.p2e_dv3.agent import build_agent
 from sheeprl.data.buffers import EnvIndependentReplayBuffer, SequentialReplayBuffer
 from sheeprl.utils.env import make_env
+from sheeprl.utils.fabric import get_single_device_fabric
 from sheeprl.utils.logger import get_log_dir, get_logger
 from sheeprl.utils.metric import MetricAggregator
 from sheeprl.utils.registry import register_algorithm
@@ -26,6 +27,9 @@ from sheeprl.utils.utils import polynomial_decay, save_configs
 
 @register_algorithm()
 def main(fabric: Fabric, cfg: Dict[str, Any], exploration_cfg: Dict[str, Any]):
+    # Single-device fabric object
+    fabric_player = get_single_device_fabric(fabric)
+
     device = fabric.device
     rank = fabric.global_rank
     world_size = fabric.world_size
@@ -150,14 +154,14 @@ def main(fabric: Fabric, cfg: Dict[str, Any], exploration_cfg: Dict[str, Any]):
 
     # initialize the ensembles with different seeds to be sure they have different weights
     player = PlayerDV3(
-        world_model.encoder.module,
+        fabric,
+        world_model.encoder,
         world_model.rssm,
-        actor_exploration.module if cfg.algo.player.actor_type == "exploration" else actor_task.module,
+        actor_exploration if cfg.algo.player.actor_type == "exploration" else actor_task,
         actions_dim,
         cfg.env.num_envs,
         cfg.algo.world_model.stochastic_size,
         cfg.algo.world_model.recurrent_model.recurrent_state_size,
-        fabric.device,
         discrete_size=cfg.algo.world_model.discrete_size,
         actor_type=cfg.algo.player.actor_type,
     )
@@ -279,10 +283,10 @@ def main(fabric: Fabric, cfg: Dict[str, Any], exploration_cfg: Dict[str, Any]):
     for update in range(start_step, num_updates + 1):
         policy_step += cfg.env.num_envs * world_size
 
-        # Measure environment interaction time: this considers both the model forward
-        # to get the action given the observation and the time taken into the environment
-        with timer("Time/env_interaction_time", SumMetric, sync_on_compute=False):
-            with torch.no_grad():
+        with torch.inference_mode():
+            # Measure environment interaction time: this considers both the model forward
+            # to get the action given the observation and the time taken into the environment
+            with timer("Time/env_interaction_time", SumMetric, sync_on_compute=False):
                 preprocessed_obs = {}
                 for k, v in obs.items():
                     preprocessed_obs[k] = torch.as_tensor(v[np.newaxis], dtype=torch.float32, device=device)
@@ -300,76 +304,78 @@ def main(fabric: Fabric, cfg: Dict[str, Any], exploration_cfg: Dict[str, Any]):
                         torch.cat([real_act.argmax(dim=-1) for real_act in real_actions], dim=-1).cpu().numpy()
                     )
 
-            step_data["actions"] = actions.reshape((1, cfg.env.num_envs, -1))
-            rb.add(step_data, validate_args=cfg.buffer.validate_args)
+                step_data["actions"] = actions.reshape((1, cfg.env.num_envs, -1))
+                rb.add(step_data, validate_args=cfg.buffer.validate_args)
 
-            next_obs, rewards, dones, truncated, infos = envs.step(real_actions.reshape(envs.action_space.shape))
-            dones = np.logical_or(dones, truncated).astype(np.uint8)
+                next_obs, rewards, dones, truncated, infos = envs.step(real_actions.reshape(envs.action_space.shape))
+                dones = np.logical_or(dones, truncated).astype(np.uint8)
 
-        step_data["is_first"] = np.zeros_like(step_data["dones"])
-        if "restart_on_exception" in infos:
-            for i, agent_roe in enumerate(infos["restart_on_exception"]):
-                if agent_roe and not dones[i]:
-                    last_inserted_idx = (rb.buffer[i]._pos - 1) % rb.buffer[i].buffer_size
-                    rb.buffer[i]["dones"][last_inserted_idx] = np.ones_like(rb.buffer[i]["dones"][last_inserted_idx])
-                    rb.buffer[i]["is_first"][last_inserted_idx] = np.zeros_like(
-                        rb.buffer[i]["is_first"][last_inserted_idx]
-                    )
-                    step_data["is_first"][i] = np.ones_like(step_data["is_first"][i])
+            step_data["is_first"] = np.zeros_like(step_data["dones"])
+            if "restart_on_exception" in infos:
+                for i, agent_roe in enumerate(infos["restart_on_exception"]):
+                    if agent_roe and not dones[i]:
+                        last_inserted_idx = (rb.buffer[i]._pos - 1) % rb.buffer[i].buffer_size
+                        rb.buffer[i]["dones"][last_inserted_idx] = np.ones_like(
+                            rb.buffer[i]["dones"][last_inserted_idx]
+                        )
+                        rb.buffer[i]["is_first"][last_inserted_idx] = np.zeros_like(
+                            rb.buffer[i]["is_first"][last_inserted_idx]
+                        )
+                        step_data["is_first"][i] = np.ones_like(step_data["is_first"][i])
 
-        if cfg.metric.log_level > 0 and "final_info" in infos:
-            for i, agent_ep_info in enumerate(infos["final_info"]):
-                if agent_ep_info is not None:
-                    ep_rew = agent_ep_info["episode"]["r"]
-                    ep_len = agent_ep_info["episode"]["l"]
-                    if aggregator and not aggregator.disabled:
-                        aggregator.update("Rewards/rew_avg", ep_rew)
-                        aggregator.update("Game/ep_len_avg", ep_len)
-                    fabric.print(f"Rank-0: policy_step={policy_step}, reward_env_{i}={ep_rew[-1]}")
+            if cfg.metric.log_level > 0 and "final_info" in infos:
+                for i, agent_ep_info in enumerate(infos["final_info"]):
+                    if agent_ep_info is not None:
+                        ep_rew = agent_ep_info["episode"]["r"]
+                        ep_len = agent_ep_info["episode"]["l"]
+                        if aggregator and not aggregator.disabled:
+                            aggregator.update("Rewards/rew_avg", ep_rew)
+                            aggregator.update("Game/ep_len_avg", ep_len)
+                        fabric.print(f"Rank-0: policy_step={policy_step}, reward_env_{i}={ep_rew[-1]}")
 
-        # Save the real next observation
-        real_next_obs = copy.deepcopy(next_obs)
-        if "final_observation" in infos:
-            for idx, final_obs in enumerate(infos["final_observation"]):
-                if final_obs is not None:
-                    for k, v in final_obs.items():
-                        real_next_obs[k][idx] = v
+            # Save the real next observation
+            real_next_obs = copy.deepcopy(next_obs)
+            if "final_observation" in infos:
+                for idx, final_obs in enumerate(infos["final_observation"]):
+                    if final_obs is not None:
+                        for k, v in final_obs.items():
+                            real_next_obs[k][idx] = v
 
-        for k in obs_keys:
-            step_data[k] = next_obs[k][np.newaxis]
-
-        # next_obs becomes the new obs
-        obs = next_obs
-
-        rewards = rewards.reshape((1, cfg.env.num_envs, -1))
-        step_data["dones"] = dones.reshape((1, cfg.env.num_envs, -1))
-        step_data["rewards"] = clip_rewards_fn(rewards)
-
-        dones_idxes = dones.nonzero()[0].tolist()
-        reset_envs = len(dones_idxes)
-        if reset_envs > 0:
-            reset_data = {}
             for k in obs_keys:
-                reset_data[k] = (real_next_obs[k][dones_idxes])[np.newaxis]
-            reset_data["dones"] = np.ones((1, reset_envs, 1))
-            reset_data["actions"] = np.zeros((1, reset_envs, np.sum(actions_dim)))
-            reset_data["rewards"] = step_data["rewards"][:, dones_idxes]
-            reset_data["is_first"] = np.zeros_like(reset_data["dones"])
-            rb.add(reset_data, dones_idxes, validate_args=cfg.buffer.validate_args)
+                step_data[k] = next_obs[k][np.newaxis]
 
-            # Reset already inserted step data
-            step_data["rewards"][:, dones_idxes] = np.zeros_like(reset_data["rewards"])
-            step_data["dones"][:, dones_idxes] = np.zeros_like(step_data["dones"][:, dones_idxes])
-            step_data["is_first"][:, dones_idxes] = np.ones_like(step_data["is_first"][:, dones_idxes])
-            player.init_states(dones_idxes)
+            # next_obs becomes the new obs
+            obs = next_obs
+
+            rewards = rewards.reshape((1, cfg.env.num_envs, -1))
+            step_data["dones"] = dones.reshape((1, cfg.env.num_envs, -1))
+            step_data["rewards"] = clip_rewards_fn(rewards)
+
+            dones_idxes = dones.nonzero()[0].tolist()
+            reset_envs = len(dones_idxes)
+            if reset_envs > 0:
+                reset_data = {}
+                for k in obs_keys:
+                    reset_data[k] = (real_next_obs[k][dones_idxes])[np.newaxis]
+                reset_data["dones"] = np.ones((1, reset_envs, 1))
+                reset_data["actions"] = np.zeros((1, reset_envs, np.sum(actions_dim)))
+                reset_data["rewards"] = step_data["rewards"][:, dones_idxes]
+                reset_data["is_first"] = np.zeros_like(reset_data["dones"])
+                rb.add(reset_data, dones_idxes, validate_args=cfg.buffer.validate_args)
+
+                # Reset already inserted step data
+                step_data["rewards"][:, dones_idxes] = np.zeros_like(reset_data["rewards"])
+                step_data["dones"][:, dones_idxes] = np.zeros_like(step_data["dones"][:, dones_idxes])
+                step_data["is_first"][:, dones_idxes] = np.ones_like(step_data["is_first"][:, dones_idxes])
+                player.init_states(dones_idxes)
 
         updates_before_training -= 1
 
         # Train the agent
         if update >= learning_starts and updates_before_training <= 0:
             if player.actor_type == "exploration":
-                player.actor = actor_task.module
                 player.actor_type = "task"
+                player.actor = fabric_player.setup_module(getattr(actor_task, "module", actor_task))
             local_data = rb.sample_tensors(
                 cfg.algo.per_rank_batch_size,
                 sequence_length=cfg.algo.per_rank_sequence_length,
@@ -485,10 +491,11 @@ def main(fabric: Fabric, cfg: Dict[str, Any], exploration_cfg: Dict[str, Any]):
             )
 
     envs.close()
+
     # task test few-shot
     if fabric.is_global_zero and cfg.algo.run_test:
-        player.actor = actor_task.module
         player.actor_type = "task"
+        player.actor = fabric_player.setup_module(getattr(actor_task, "module", actor_task))
         test(player, fabric, cfg, log_dir, "few-shot")
 
     if not cfg.model_manager.disabled and fabric.is_global_zero:

sheeprl/algos/ppo/agent.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import copy
 from math import prod
 from typing import Any, Dict, List, Optional, Sequence, Tuple
 
@@ -7,12 +8,12 @@ import gymnasium
 import torch
 import torch.nn as nn
 from lightning import Fabric
-from lightning.fabric.wrappers import _FabricModule
 from torch import Tensor
 from torch.distributions import Distribution, Independent, Normal
 
 from sheeprl.models.models import MLP, MultiEncoder, NatureCNN
 from sheeprl.utils.distribution import OneHotCategoricalValidateArgs
+from sheeprl.utils.fabric import get_single_device_fabric
 
 
 class CNNEncoder(nn.Module):
@@ -63,6 +64,18 @@ class MLPEncoder(nn.Module):
         return self.model(x)
 
 
+class PPOActor(nn.Module):
+    def __init__(self, actor_backbone: torch.nn.Module, actor_heads: torch.nn.ModuleList, is_continuous: bool) -> None:
+        super().__init__()
+        self.actor_backbone = actor_backbone
+        self.actor_heads = actor_heads
+        self.is_continuous = is_continuous
+
+    def forward(self, x: Tensor) -> List[Tensor]:
+        x = self.actor_backbone(x)
+        return [head(x) for head in self.actor_heads]
+
+
 class PPOAgent(nn.Module):
     def __init__(
         self,
@@ -78,6 +91,7 @@ class PPOAgent(nn.Module):
         is_continuous: bool = False,
     ):
         super().__init__()
+        self.is_continuous = is_continuous
         self.distribution_cfg = distribution_cfg
         self.actions_dim = actions_dim
         in_channels = sum([prod(obs_space[k].shape[:-2]) for k in cnn_keys])
@@ -101,7 +115,6 @@ class PPOAgent(nn.Module):
             else None
         )
         self.feature_extractor = MultiEncoder(cnn_encoder, mlp_encoder)
-        self.is_continuous = is_continuous
         features_dim = self.feature_extractor.output_dim
         self.critic = MLP(
             input_dims=features_dim,
@@ -115,7 +128,7 @@ class PPOAgent(nn.Module):
                 else None
             ),
         )
-        self.actor_backbone = (
+        actor_backbone = (
             MLP(
                 input_dims=features_dim,
                 output_dim=None,
@@ -133,21 +146,19 @@ class PPOAgent(nn.Module):
             else nn.Identity()
         )
         if is_continuous:
-            self.actor_heads = nn.ModuleList([nn.Linear(actor_cfg.dense_units, sum(actions_dim) * 2)])
+            actor_heads = nn.ModuleList([nn.Linear(actor_cfg.dense_units, sum(actions_dim) * 2)])
         else:
-            self.actor_heads = nn.ModuleList(
-                [nn.Linear(actor_cfg.dense_units, action_dim) for action_dim in actions_dim]
-            )
+            actor_heads = nn.ModuleList([nn.Linear(actor_cfg.dense_units, action_dim) for action_dim in actions_dim])
+        self.actor = PPOActor(actor_backbone, actor_heads, is_continuous)
 
     def forward(
-        self, obs: Dict[str, Tensor], actions: Optional[List[Tensor]] = None
+        self, obs: Dict[str, Tensor], actions: Optional[List[Tensor]] = None, greedy: bool = False
     ) -> Tuple[Sequence[Tensor], Tensor, Tensor, Tensor]:
         feat = self.feature_extractor(obs)
-        out: Tensor = self.actor_backbone(feat)
-        pre_dist: List[Tensor] = [head(out) for head in self.actor_heads]
         values = self.critic(feat)
+        actor_out: List[Tensor] = self.actor(feat)
         if self.is_continuous:
-            mean, log_std = torch.chunk(pre_dist[0], chunks=2, dim=-1)
+            mean, log_std = torch.chunk(actor_out[0], chunks=2, dim=-1)
             std = log_std.exp()
             normal = Independent(
                 Normal(mean, std, validate_args=self.distribution_cfg.validate_args),
@@ -155,7 +166,10 @@ class PPOAgent(nn.Module):
                 validate_args=self.distribution_cfg.validate_args,
             )
             if actions is None:
-                actions = normal.sample()
+                if greedy:
+                    actions = mean
+                else:
+                    actions = normal.sample()
             else:
                 # always composed by a tuple of one element containing all the
                 # continuous actions
@@ -170,13 +184,16 @@ class PPOAgent(nn.Module):
             if actions is None:
                 should_append = True
                 actions: List[Tensor] = []
-            for i, logits in enumerate(pre_dist):
+            for i, logits in enumerate(actor_out):
                 actions_dist.append(
                     OneHotCategoricalValidateArgs(logits=logits, validate_args=self.distribution_cfg.validate_args)
                 )
                 actions_entropies.append(actions_dist[-1].entropy())
                 if should_append:
-                    actions.append(actions_dist[-1].sample())
+                    if greedy:
+                        actions.append(actions_dist[-1].mode)
+                    else:
+                        actions.append(actions_dist[-1].sample())
                 actions_logprobs.append(actions_dist[-1].log_prob(actions[i]))
             return (
                 tuple(actions),
@@ -189,20 +206,6 @@ class PPOAgent(nn.Module):
         feat = self.feature_extractor(obs)
         return self.critic(feat)
 
-    def get_greedy_actions(self, obs: Dict[str, Tensor]) -> Sequence[Tensor]:
-        feat = self.feature_extractor(obs)
-        out = self.actor_backbone(feat)
-        pre_dist: List[Tensor] = [head(out) for head in self.actor_heads]
-        if self.is_continuous:
-            return [torch.chunk(pre_dist[0], 2, -1)[0]]
-        else:
-            return tuple(
-                [
-                    OneHotCategoricalValidateArgs(logits=logits, validate_args=self.distribution_cfg.validate_args).mode
-                    for logits in pre_dist
-                ]
-            )
-
 
 def build_agent(
     fabric: Fabric,
@@ -211,7 +214,7 @@ def build_agent(
     cfg: Dict[str, Any],
     obs_space: gymnasium.spaces.Dict,
     agent_state: Optional[Dict[str, Tensor]] = None,
-) -> _FabricModule:
+) -> Tuple[PPOAgent, PPOAgent]:
     agent = PPOAgent(
         actions_dim=actions_dim,
         obs_space=obs_space,
@@ -226,6 +229,16 @@ def build_agent(
     )
     if agent_state:
         agent.load_state_dict(agent_state)
+    player = copy.deepcopy(agent)
+
+    # Setup training agent
     agent = fabric.setup_module(agent)
 
-    return agent
+    # Setup player agent
+    fabric_player = get_single_device_fabric(fabric)
+    player = fabric_player.setup_module(player)
+
+    # Tie weights between the agent and the player
+    for agent_p, player_p in zip(agent.parameters(), player.parameters()):
+        player_p.data = agent_p.data
+    return agent, player

sheeprl/algos/sac_ae/agent.py
@@ -15,6 +15,7 @@ from torch import Size, Tensor
 
 from sheeprl.algos.sac_ae.utils import weight_init
 from sheeprl.models.models import CNN, MLP, DeCNN, MultiDecoder, MultiEncoder
+from sheeprl.utils.fabric import get_single_device_fabric
 from sheeprl.utils.model import cnn_forward
 
 LOG_STD_MAX = 2
@@ -73,6 +74,9 @@ class CNNEncoder(CNN):
         return self._conv_output_shape
 
     def forward(self, obs: Dict[str, Tensor], *, detach_encoder_features: bool = False, **kwargs) -> Tensor:
+        dtypes = [obs[k].dtype for k in self.keys]
+        if dtypes.count(dtypes[0]) != len(dtypes):
+            raise ValueError("All the tensors must have the same dtype: {}".format(dtypes))
         x = torch.cat([obs[k] for k in self.keys], dim=-3)
         x = cnn_forward(self.model, x, x.shape[-3:], (-1,))
         if detach_encoder_features:
@@ -104,7 +108,10 @@ class MLPEncoder(nn.Module):
         self.input_dim = input_dim
 
     def forward(self, obs: Dict[str, Tensor], *args, detach_encoder_features: bool = False, **kwargs) -> Tensor:
-        x = torch.cat([obs[k] for k in self.keys], dim=-1).type(torch.float32)
+        dtypes = [obs[k].dtype for k in self.keys]
+        if dtypes.count(dtypes[0]) != len(dtypes):
+            raise ValueError("All the tensors must have the same dtype: {}".format(dtypes))
+        x = torch.cat([obs[k] for k in self.keys], dim=-1)
         x = self.model(x)
         if detach_encoder_features:
             x = x.detach()
@@ -254,7 +261,9 @@ class SACAEContinuousActor(nn.Module):
         # Orthogonal init
         self.apply(weight_init)
 
-    def forward(self, obs: Tensor, detach_encoder_features: bool = False) -> Tuple[Tensor, Tensor]:
+    def forward(
+        self, obs: Tensor, detach_encoder_features: bool = False, greedy: bool = False
+    ) -> Union[Tensor, Tuple[Tensor, Tensor]]:
         """Given an observation, it returns a tanh-squashed
         sampled action (correctly rescaled to the environment action bounds) and its
         log-prob (as defined in Eq. 26 of https://arxiv.org/abs/1812.05905)
@@ -269,11 +278,14 @@ class SACAEContinuousActor(nn.Module):
         features = self.encoder(obs, detach_encoder_features=detach_encoder_features)
         x = self.model(features)
         mean = self.fc_mean(x)
-        log_std = self.fc_logstd(x)
-        # log_std = torch.clamp(log_std, LOG_STD_MIN, LOG_STD_MAX)
-        log_std = torch.tanh(log_std)
-        log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)
-        return self.get_actions_and_log_probs(mean, log_std.exp())
+        if greedy:
+            return torch.tanh(mean) * self.action_scale + self.action_bias
+        else:
+            log_std = self.fc_logstd(x)
+            # log_std = torch.clamp(log_std, LOG_STD_MIN, LOG_STD_MAX)
+            log_std = torch.tanh(log_std)
+            log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)
+            return self.get_actions_and_log_probs(mean, log_std.exp())
 
     def get_actions_and_log_probs(self, mean: Tensor, std: Tensor):
         """Given the mean and the std of a Normal distribution, it returns a tanh-squashed
@@ -308,21 +320,6 @@ class SACAEContinuousActor(nn.Module):
         log_prob = log_prob.sum(-1, keepdim=True)
 
         return action, log_prob
-
-    def get_greedy_actions(self, obs: Tensor) -> Tensor:
-        """Get the action given the input observation greedily
-
-        Args:
-            obs (Tensor): input observation
-
-        Returns:
-            action
-        """
-        features = self.encoder(obs)
-        x = self.model(features)
-        mean = self.fc_mean(x)
-        mean = torch.tanh(mean) * self.action_scale + self.action_bias
-        return mean
 
 
 class SACAEAgent(nn.Module):
@@ -407,6 +404,11 @@ class SACAEAgent(nn.Module):
     @property
     def critic_target(self) -> SACAECritic:
         return self._critic_target
+
+    @critic_target.setter
+    def critic_target(self, critic_target: SACAECritic | _FabricModule) -> None:
+        self._critic_target = critic_target
+        return
 
     @property
     def alpha(self) -> float:
@@ -563,5 +565,10 @@ def build_agent(
     decoder = fabric.setup_module(decoder)
     agent.actor = fabric.setup_module(agent.actor)
     agent.critic = fabric.setup_module(agent.critic)
+
+    # Wrap the target critic with a single-device fabric. This lets the target critic
+    # to be on the same device as the agent and to run with the same precision
+    fabric_player = get_single_device_fabric(fabric)
+    agent.critic_target = fabric_player.setup_module(agent.critic_target)
 
     return agent, encoder, decoder

sheeprl/algos/sac_ae/sac_ae.py
@@ -26,6 +26,7 @@ from sheeprl.algos.sac_ae.utils import preprocess_obs, test
 from sheeprl.data.buffers import ReplayBuffer
 from sheeprl.models.models import MultiDecoder, MultiEncoder
 from sheeprl.utils.env import make_env
+from sheeprl.utils.fabric import get_single_device_fabric
 from sheeprl.utils.logger import get_log_dir, get_logger
 from sheeprl.utils.metric import MetricAggregator
 from sheeprl.utils.registry import register_algorithm
@@ -207,6 +208,8 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
         state["encoder"] if cfg.checkpoint.resume_from else None,
         state["decoder"] if cfg.checkpoint.resume_from else None,
     )
+    fabric_player = get_single_device_fabric(fabric)
+    actor = fabric_player.setup_module(agent.actor.module)
 
     # Optimizers
     qf_optimizer = hydra.utils.instantiate(cfg.algo.critic.optimizer, params=agent.critic.parameters(), _convert_="all")
@@ -313,10 +316,10 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
             if update < learning_starts:
                 actions = envs.action_space.sample()
             else:
-                with torch.no_grad():
+                with torch.inference_mode():
                     normalized_obs = {k: v / 255 if k in cfg.algo.cnn_keys.encoder else v for k, v in obs.items()}
                     torch_obs = {k: torch.from_numpy(v).to(device).float() for k, v in normalized_obs.items()}
-                    actions, _ = agent.actor.module(torch_obs)
+                    actions, _ = actor(torch_obs)
                     actions = actions.cpu().numpy()
             next_obs, rewards, dones, truncated, infos = envs.step(actions.reshape(envs.action_space.shape))
             dones = np.logical_or(dones, truncated)
@@ -471,7 +474,7 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
 
     envs.close()
     if fabric.is_global_zero and cfg.algo.run_test:
-        test(agent.actor.module, fabric, cfg, log_dir)
+        test(actor, fabric, cfg, log_dir)
 
     if not cfg.model_manager.disabled and fabric.is_global_zero:
         from sheeprl.algos.sac_ae.utils import log_models