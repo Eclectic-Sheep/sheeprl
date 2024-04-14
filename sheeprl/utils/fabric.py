8388044fe8c54394dd81adbee3615d5f9a64102f
belerico <belo.fede@outlook.com>
2024-04-14 19:21:17 +0200

    Add SingleDeviceDDPStrategy

--- sheeprl/utils/fabric.py
+++ sheeprl/utils/fabric.py
@@ -1,8 +1,19 @@
+from datetime import timedelta
+from typing import Any, Literal, Optional
 from unittest import mock
 
+import torch
+import torch.distributed
 from lightning.fabric import Fabric
 from lightning.fabric.accelerators import XLAAccelerator
+from lightning.fabric.accelerators.accelerator import Accelerator
+from lightning.fabric.plugins.collectives.torch_collective import default_pg_timeout
+from lightning.fabric.plugins.environments.cluster_environment import ClusterEnvironment
+from lightning.fabric.plugins.io.checkpoint_io import CheckpointIO
+from lightning.fabric.plugins.precision import Precision
 from lightning.fabric.strategies import SingleDeviceStrategy, SingleDeviceXLAStrategy
+from lightning.fabric.strategies.ddp import DDPStrategy
+from lightning.fabric.strategies.registry import _StrategyRegistry
 
 
 def get_single_device_fabric(fabric: Fabric) -> Fabric:
@@ -33,3 +44,53 @@ def get_single_device_fabric(fabric: Fabric) -> Fabric:
         mocked_os_environ.pop("LT_ACCELERATOR", None)
         fabric = Fabric(strategy=strategy)
     return fabric
+
+
+class SingleDeviceDDPStrategy(DDPStrategy):
+    """Strategy for multi-process single-device training on one or multiple nodes."""
+
+    def __init__(
+        self,
+        accelerator: Optional[Accelerator] = None,
+        parallel_devices: Optional[int] = None,
+        cluster_environment: Optional[ClusterEnvironment] = None,
+        checkpoint_io: Optional[CheckpointIO] = None,
+        precision: Optional[Precision] = None,
+        process_group_backend: Optional[str] = None,
+        timeout: Optional[timedelta] = default_pg_timeout,
+        start_method: Literal["popen", "spawn", "fork", "forkserver"] = "popen",
+        **kwargs: Any,
+    ) -> None:
+        if parallel_devices is None:
+            parallel_devices = 1
+        elif not isinstance(parallel_devices, int):
+            raise ValueError("parallel_devices must be an integer.")
+        process_group_backend = "gloo"
+        parallel_devices = [torch.device("cuda:0") for _ in range(parallel_devices)]
+        super().__init__(
+            accelerator=accelerator,
+            parallel_devices=parallel_devices,
+            cluster_environment=cluster_environment,
+            checkpoint_io=checkpoint_io,
+            precision=precision,
+            process_group_backend=process_group_backend,
+            timeout=timeout,
+            start_method=start_method,
+            **kwargs,
+        )
+
+    @classmethod
+    def register_strategies(cls, strategy_registry: _StrategyRegistry) -> None:
+        entries = (
+            ("single_ddp", "popen"),
+            ("single_ddp_spawn", "spawn"),
+            ("single_ddp_fork", "fork"),
+            ("single_ddp_notebook", "fork"),
+        )
+        for name, start_method in entries:
+            strategy_registry.register(
+                name,
+                cls,
+                description=f"DDP strategy with `start_method={start_method!r}` and a single device on GLOO.",
+                start_method=start_method,
+            )
