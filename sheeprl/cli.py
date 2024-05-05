56d25f25762074738b6d0dfb873ff91e8a3c46d7
belerico <belo.fede@outlook.com>
2024-05-05 20:14:16 +0200

    Fix check algo.learning_starts

--- sheeprl/cli.py
+++ sheeprl/cli.py
@@ -337,7 +337,7 @@ def check_configs(cfg: Dict[str, Any]):
             UserWarning,
         )
         cfg.model_manager.disabled = True
-    if cfg.algo.learning_starts < 0:
+    if cfg.algo.learning_starts is not None and cfg.algo.learning_starts < 0:
         raise ValueError("The `algo.learning_starts` parameter must be greater or equal to zero.")
 
 
