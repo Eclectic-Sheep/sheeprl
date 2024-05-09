ef7ef3096816aada7bdb73cc615189d2821686fa
belerico <belo.fede@outlook.com>
2024-05-09 08:38:47 +0200

    ratio_steps account every time for the policy_steps_per_update

--- sheeprl/algos/p2e_dv3/p2e_dv3_exploration.py
+++ sheeprl/algos/p2e_dv3/p2e_dv3_exploration.py
@@ -895,9 +895,7 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
 
         # Train the agent
         if update >= learning_starts:
-            ratio_steps = policy_step - prefill_steps
-            if update == learning_starts:
-                ratio_steps += policy_steps_per_update
+            ratio_steps = policy_step - prefill_steps + policy_steps_per_update
             per_rank_gradient_steps = ratio(ratio_steps / world_size)
             if per_rank_gradient_steps > 0:
                 local_data = rb.sample_tensors(
