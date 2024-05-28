169d14aec91c560a9018865079f06b213f76a34e
Michele Milesi <74559684+michele-milesi@users.noreply.github.com>
2024-05-28 18:03:51 +0200

    fix: policy steps update in on-policy algorithms (#293)

--- sheeprl/algos/ppo/ppo.py
+++ sheeprl/algos/ppo/ppo.py
@@ -265,7 +265,7 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
     for iter_num in range(start_iter, total_iters + 1):
         with torch.inference_mode():
             for _ in range(0, cfg.algo.rollout_steps):
-                policy_step += policy_steps_per_iter
+                policy_step += cfg.env.num_envs * world_size
 
                 # Measure environment interaction time: this considers both the model forward
                 # to get the action given the observation and the time taken into the environment
