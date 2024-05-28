db802b141c62ce6fec5c12055ebdcf6f1df98a38
Michele Milesi <michele.milesi@orobix.com>
2024-05-28 17:26:39 +0200

    fix: policy steps update in on-policy algorithms

--- howto/register_external_algorithm.md
+++ howto/register_external_algorithm.md
@@ -592,7 +592,7 @@ def ext_sota_main(fabric: Fabric, cfg: Dict[str, Any]):
 
     for iter_num in range(start_iter, total_iters + 1):
         for _ in range(0, cfg.algo.rollout_steps):
-            policy_step += policy_steps_per_iter
+            policy_step += cfg.env.num_envs * world_size
 
             # Measure environment interaction time: this considers both the model forward
             # to get the action given the observation and the time taken into the environment
--- sheeprl/algos/a2c/a2c.py
+++ sheeprl/algos/a2c/a2c.py
@@ -228,7 +228,7 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
     for iter_num in range(1, total_iters + 1):
         with torch.inference_mode():
             for _ in range(0, cfg.algo.rollout_steps):
-                policy_step += policy_steps_per_iter
+                policy_step += cfg.env.num_envs * world_size
 
                 # Measure environment interaction time: this considers both the model forward
                 # to get the action given the observation and the time taken into the environment
