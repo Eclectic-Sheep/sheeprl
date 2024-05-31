dee8c8010bdff9bc3fd81669ffd4c90513f9b578
Locatelli Alex <116915251+LocaAlex@users.noreply.github.com>
2024-05-31 18:29:54 +0200

    Tests/Added tests for env wrappers (#296)
    * Tests/Added tests for env wrappers
    
    * Tests/Deleted unnecessary file
    
    * Tests/Minor fixes to previous commit
    
    ---------
    
    Co-authored-by: Locatelli Alex Giannino <alex.locatelli@orobix.com>
    Co-authored-by: Michele Milesi <74559684+michele-milesi@users.noreply.github.com>

--- tests/test_envs/test_wrappers.py
+++ tests/test_envs/test_wrappers.py
@@ -3,7 +3,7 @@ import numpy as np
 import pytest
 
 from sheeprl.envs.dummy import ContinuousDummyEnv, DiscreteDummyEnv, MultiDiscreteDummyEnv
-from sheeprl.envs.wrappers import ActionsAsObservationWrapper, MaskVelocityWrapper
+from sheeprl.envs.wrappers import ActionRepeat, MaskVelocityWrapper, RewardAsObservationWrapper
 
 ENVIRONMENTS = {
     "discrete_dummy": DiscreteDummyEnv,
@@ -18,85 +18,87 @@ def test_mask_velocities_fail():
         env = MaskVelocityWrapper(env)
 
 
-@pytest.mark.parametrize("num_stack", [1, 4, 8])
-@pytest.mark.parametrize("dilation", [1, 2, 4])
-@pytest.mark.parametrize("env_id", ["discrete_dummy", "multidiscrete_dummy", "continuous_dummy"])
-def test_actions_as_observation_wrapper(env_id: str, num_stack, dilation):
-    env = ENVIRONMENTS[env_id]()
-    if isinstance(env.action_space, gym.spaces.MultiDiscrete):
-        noop = [0, 0]
+@pytest.mark.parametrize("env_id", ENVIRONMENTS.keys())
+@pytest.mark.parametrize("dict_obs_space", [True, False])
+def test_rewards_as_observation_wrapper_initialization(env_id, dict_obs_space):
+    env = ENVIRONMENTS[env_id](dict_obs_space=dict_obs_space)
+    wrapped_env = RewardAsObservationWrapper(env)
+
+    if dict_obs_space:
+        assert "reward" in wrapped_env.observation_space.spaces
+        assert isinstance(wrapped_env.observation_space.spaces["reward"], gym.spaces.Box)
     else:
-        noop = 0
-    env = ActionsAsObservationWrapper(env, num_stack=num_stack, noop=noop, dilation=dilation)
-
-    o = env.reset()[0]
-    assert len(o["action_stack"].shape) == len(env.observation_space["action_stack"].shape)
-    for d1, d2 in zip(o["action_stack"].shape, env.observation_space["action_stack"].shape):
-        assert d1 == d2
-
-    for _ in range(64):
-        o = env.step(env.action_space.sample())[0]
-        assert len(o["action_stack"].shape) == len(env.observation_space["action_stack"].shape)
-        for d1, d2 in zip(o["action_stack"].shape, env.observation_space["action_stack"].shape):
-            assert d1 == d2
+        assert isinstance(wrapped_env.observation_space, gym.spaces.Dict)
+        assert "obs" in wrapped_env.observation_space.spaces
+        assert "reward" in wrapped_env.observation_space.spaces
+        assert isinstance(wrapped_env.observation_space.spaces["reward"], gym.spaces.Box)
 
 
-@pytest.mark.parametrize("num_stack", [-1, 0])
-@pytest.mark.parametrize("env_id", ["discrete_dummy", "multidiscrete_dummy", "continuous_dummy"])
-def test_actions_as_observation_wrapper_invalid_num_stack(env_id, num_stack):
-    env = ENVIRONMENTS[env_id]()
-    if isinstance(env.action_space, gym.spaces.MultiDiscrete):
-        noop = [0, 0]
+@pytest.mark.parametrize("env_id", ENVIRONMENTS.keys())
+@pytest.mark.parametrize("dict_obs_space", [True, False])
+def test_rewards_as_observation_wrapper_step_method(env_id, dict_obs_space):
+    env = ENVIRONMENTS[env_id](dict_obs_space=dict_obs_space)
+    wrapped_env = RewardAsObservationWrapper(env)
+
+    obs = wrapped_env.step(env.action_space.sample())[0]
+    if dict_obs_space:
+        assert "rgb" in obs
+        assert "state" in obs
+        assert "reward" in obs
     else:
-        noop = 0
-    with pytest.raises(ValueError, match="The number of actions to the"):
-        env = ActionsAsObservationWrapper(env, num_stack=num_stack, noop=noop, dilation=3)
+        assert "obs" in obs
+        assert "reward" in obs
+    np.testing.assert_array_equal(obs["reward"], np.array([0.0]))
 
 
-@pytest.mark.parametrize("dilation", [-1, 0])
-@pytest.mark.parametrize("env_id", ["discrete_dummy", "multidiscrete_dummy", "continuous_dummy"])
-def test_actions_as_observation_wrapper_invalid_dilation(env_id, dilation):
-    env = ENVIRONMENTS[env_id]()
-    if isinstance(env.action_space, gym.spaces.MultiDiscrete):
-        noop = [0, 0]
+@pytest.mark.parametrize("env_id", ENVIRONMENTS.keys())
+@pytest.mark.parametrize("dict_obs_space", [True, False])
+def test_rewards_as_observation_wrapper_reset_method(env_id, dict_obs_space):
+    env = ENVIRONMENTS[env_id](dict_obs_space=dict_obs_space)
+    wrapped_env = RewardAsObservationWrapper(env)
+
+    obs = wrapped_env.reset()[0]
+    if dict_obs_space:
+        assert "rgb" in obs
+        assert "state" in obs
+        assert "reward" in obs
     else:
-        noop = 0
-    with pytest.raises(ValueError, match="The actions stack dilation argument must be greater than zero"):
-        env = ActionsAsObservationWrapper(env, num_stack=3, noop=noop, dilation=dilation)
+        assert "obs" in obs
+        assert "reward" in obs
+    np.testing.assert_array_equal(obs["reward"], np.array([0.0]))
 
 
-@pytest.mark.parametrize("noop", [set([0, 0, 0]), "this is an invalid type", np.array([0, 0, 0])])
-@pytest.mark.parametrize("env_id", ["discrete_dummy", "multidiscrete_dummy", "continuous_dummy"])
-def test_actions_as_observation_wrapper_invalid_noop_type(env_id, noop):
+@pytest.mark.parametrize("amount", [-1.3, -1, 0])
+@pytest.mark.parametrize("env_id", ENVIRONMENTS.keys())
+def test_action_repeat_non_positive_amount(env_id, amount):
     env = ENVIRONMENTS[env_id]()
-    with pytest.raises(ValueError, match="The noop action must be an integer or float or list"):
-        env = ActionsAsObservationWrapper(env, num_stack=3, noop=noop, dilation=2)
+    with pytest.raises(ValueError, match="`amount` should be a positive integer"):
+        env = ActionRepeat(env, amount)
 
 
-def test_actions_as_observation_wrapper_invalid_noop_continuous_type():
-    env = ContinuousDummyEnv()
-    with pytest.raises(ValueError, match="The noop actions must be a float for continuous action spaces"):
-        env = ActionsAsObservationWrapper(env, num_stack=3, noop=[0, 0, 0], dilation=2)
+@pytest.mark.parametrize("amount", [1, 2, 3, 7, 10])
+@pytest.mark.parametrize("env_id", ENVIRONMENTS.keys())
+def test_action_repeat(env_id: str, amount):
+    env = ENVIRONMENTS[env_id]()
+    env = ActionRepeat(env, amount)
+
+    env.reset()
+
+    assert env.action_repeat == amount
+    for i in range(amount * 10):
+        _, _, done, _, _ = env.step(env.action_space.sample())
+        step = env.__getattr__("_current_step")
+        if not done:
+            assert amount * (i + 1) == step
+        else:
+            assert amount * i < step <= amount * (i + 1)
+            break
 
 
-@pytest.mark.parametrize("noop", [[0, 0, 0], 0.0])
-def test_actions_as_observation_wrapper_invalid_noop_discrete_type(noop):
-    env = DiscreteDummyEnv()
-    with pytest.raises(ValueError, match="The noop actions must be an integer for discrete action spaces"):
-        env = ActionsAsObservationWrapper(env, num_stack=3, noop=noop, dilation=2)
+@pytest.mark.parametrize("env_id", ENVIRONMENTS.keys())
+def test_reset_method(env_id):
+    env = ENVIRONMENTS[env_id]()
+    env = ActionRepeat(env, amount=3)
 
-
-@pytest.mark.parametrize("noop", [0, 0.0])
-def test_actions_as_observation_wrapper_invalid_noop_multidiscrete_type(noop):
-    env = MultiDiscreteDummyEnv()
-    with pytest.raises(ValueError, match="The noop actions must be a list for multi-discrete action spaces"):
-        env = ActionsAsObservationWrapper(env, num_stack=3, noop=noop, dilation=2)
-
-
-@pytest.mark.parametrize("noop", [[0], [0, 0, 0]])
-def test_actions_as_observation_wrapper_invalid_noop_multidiscrete_n_actions(noop):
-    env = MultiDiscreteDummyEnv()
-    with pytest.raises(
-        RuntimeError, match="The number of noop actions must be equal to the number of actions of the environment"
-    ):
-        env = ActionsAsObservationWrapper(env, num_stack=3, noop=noop, dilation=2)
+    obs = env.reset()[0]
+    assert obs is not None
