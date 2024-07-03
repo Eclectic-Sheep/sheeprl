478430e070b7d123eb0e4e7dc88184dc31970d13
Michele Milesi <michele.milesi@orobix.com>
2024-07-03 22:44:26 +0200

    feat: added evaluate to mopo

--- sheeprl/__init__.py
+++ sheeprl/__init__.py
@@ -39,6 +39,7 @@ from sheeprl.algos.dreamer_v1 import evaluate as dreamer_v1_evaluate  # noqa: F4
 from sheeprl.algos.dreamer_v2 import evaluate as dreamer_v2_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.dreamer_v3 import evaluate as dreamer_v3_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.droq import evaluate as droq_evaluate  # noqa: F401, isort:skip
+from sheeprl.algos.mopo import evaluate as mopo_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.p2e_dv1 import evaluate as p2e_dv1_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.p2e_dv2 import evaluate as p2e_dv2_evaluate  # noqa: F401, isort:skip
 from sheeprl.algos.p2e_dv3 import evaluate as p2e_dv3_evaluate  # noqa: F401, isort:skip
